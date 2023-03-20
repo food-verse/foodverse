6310a6f4b52f977630f1956e9182071c2ca0e316
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-03-20 20:57:06 +0200

    Added methods for loading images

--- src/main/java/com/foodverse/utility/AssetManager.java
+++ src/main/java/com/foodverse/utility/AssetManager.java
@@ -1,10 +1,19 @@
 package com.foodverse.utility;
 
-import java.awt.*;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
+
+import javax.imageio.ImageIO;
 
 public final class AssetManager {
 
@@ -15,17 +24,44 @@ public final class AssetManager {
     public static void loadFont(String fontFamily) {
         var dir = String.format("src/main/java/com/foodverse/assets/fonts/%s/", fontFamily);
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        Stream.of(new File(dir).listFiles())
-                .map(File::getName)
-                .forEach(fontName -> {
-                    try {
-                        ge.registerFont(Font.createFont(Font.TRUETYPE_FONT,
-                                new File(String.format("%s%s", dir, fontName))));
-                    } catch (IOException | FontFormatException e) {
-                        logger.info(
-                                "Couldn't load %s font family. A fallback font will be used.");
-                    }
-                });
+        File[] fontFiles = new File(dir).listFiles();
+        if (fontFiles == null) {
+            logger.log(Level.INFO, "There is no such directory {0}", dir);
+            return;
+        }
+        Stream.of(fontFiles).map(File::getName).forEach(fontName -> {
+            try {
+                ge.registerFont(Font.createFont(Font.TRUETYPE_FONT,
+                        new File(String.format("%s%s", dir, fontName))));
+            } catch (IOException | FontFormatException e) {
+                logger.log(Level.INFO, "Could not load \"{0}\" font family.", fontFamily);
+            }
+        });
+    }
+
+    public static Optional<Image> getImage(File imageFile, int width, int height) {
+        BufferedImage bufferedImage = null;
+        Image scaledImage = null;
+        try {
+            bufferedImage = ImageIO.read(imageFile);
+            scaledImage = bufferedImage.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        } catch (IOException e) {
+            logger.log(Level.INFO, "Could not load \"{0}\" image.", imageFile);
+        }
+        return Optional.ofNullable(scaledImage);
+    }
+
+    public static Optional<Image> getImage(String source, int width, int height) {
+        BufferedImage bufferedImage = null;
+        Image scaledImage = null;
+        try {
+            URL address = new URL(source);
+            bufferedImage = ImageIO.read(address);
+            scaledImage = bufferedImage.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        } catch (IOException e) {
+            logger.log(Level.INFO, "Could not load image from \"{0}\".", source);
+        }
+        return Optional.ofNullable(scaledImage);
     }
 
 }

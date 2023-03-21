8bc6676fa8d048d9326f162ca4882c96f32fad7e
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-03-21 13:03:53 +0200

    Add state management solution

---
+++ src/main/java/com/foodverse/utility/State.java
@@ -0,0 +1,18 @@
+package com.foodverse.utility;
+
+public final class State<T> {
+
+    private T value;
+
+    public State(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+}

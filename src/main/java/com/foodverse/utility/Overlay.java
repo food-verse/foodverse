d6cba85db86e255b266fdc9d9bf43f65f628d8f3
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-03-31 16:56:35 +0300

    Prototype of Overlay class

---
+++ src/main/java/com/foodverse/utility/Overlay.java
@@ -0,0 +1,80 @@
+package com.foodverse.utility;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+/**
+ * The parent class of every overlay. Overlays extending this class get their own id and an
+ * underlying frame.
+ */
+public abstract class Overlay extends Widget implements Identifiable {
+
+    // The underlying frame holding our widgets
+    private final JFrame frame = new JFrame();
+
+    // The preferred size for the page
+    private final Dimension dimension;
+
+    /**
+     * Creates a new {@link Overlay} with the specified width and height.
+     *
+     * @param width The preferred width for the page
+     * @param height The preferred height for the page
+     */
+    protected Overlay(int width, int height) {
+        inflate(this);
+        dimension = new Dimension(width, height);
+    }
+
+    /**
+     * Creates a new {@link Overlay} with no specified dimensions.
+     */
+    protected Overlay() {
+        this(0, 0);
+    }
+
+    public String getId() {
+        return getClass().getSimpleName();
+    }
+
+    protected JFrame getFrame() {
+        return frame;
+    }
+
+    /**
+     * Adds the specified widget to the page's frame.
+     *
+     * @param widget The widget to be added
+     */
+    public void inflate(Widget widget) {
+        frame.getContentPane().add(widget.getRef());
+    }
+
+    /**
+     * It automatically sizes the window to fit its contents if the dimensions are smaller than the
+     * preferred size, and sets the window size to match the dimensions if they are greater than or
+     * equal to the preferred size. This sets the size of the frame to match the dimensions
+     * provided.
+     * <p>
+     * Finally, the setVisible() method is called on the frame object with the argument true, which
+     * makes the frame visible to the user.
+     */
+    public void open() {
+        if (dimension.getWidth() < frame.getPreferredSize().getWidth()
+                || dimension.getHeight() < frame.getPreferredSize().getHeight()) {
+            frame.pack();
+        } else {
+            frame.setSize(dimension);
+        }
+        frame.setVisible(true);
+    }
+
+    /**
+     * Hides the graphical user interface window by setting the visibility of the page's frame to
+     * false.
+     */
+    public void close() {
+        frame.setVisible(false);
+    }
+
+}

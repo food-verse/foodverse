6e9d352b20788ba01b2e4ad4a5a4b5d90391d760
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-03-31 20:30:52 +0300

    Add the supported colors

---
+++ src/main/java/com/foodverse/utility/Colors.java
@@ -0,0 +1,30 @@
+package com.foodverse.utility;
+
+import java.awt.Color;
+
+public final class Colors {
+
+    private Colors() {}
+
+    // Primary
+    public static final Color white = new Color(0xffffffff);
+    public static final Color black = new Color(0xff000000);
+
+    // Secondary
+    public static final Color blue = new Color(0xff276ef1);
+    public static final Color red = new Color(0xffd44333);
+    public static final Color orange = new Color(0xffed6e33);
+    public static final Color yellow = new Color(0xffffc043);
+    public static final Color green = new Color(0xff3aa76d);
+    public static final Color purple = new Color(0xff7356bf);
+    public static final Color brown = new Color(0xff99644c);
+
+    // Gray
+    public static final Color gray50 = new Color(0xfff6f6f6);
+    public static final Color gray100 = new Color(0xffeeeeee);
+    public static final Color gray200 = new Color(0xffe2e2e2);
+    public static final Color gray400 = new Color(0xffafafaf);
+    public static final Color gray500 = new Color(0xff757575);
+    public static final Color gray600 = new Color(0xff545454);
+
+}

2f09d78a20685fd511ec18e78550a0a2f900f043
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-01 19:00:02 +0300

    Update shop and home with the newly added buttons

--- src/main/java/com/foodverse/overlays/ShopOverlay.java
+++ src/main/java/com/foodverse/overlays/ShopOverlay.java
@@ -2,13 +2,13 @@ package com.foodverse.overlays;
 
 import java.awt.Component;
 import javax.swing.JPanel;
-import com.foodverse.utility.Button;
 import com.foodverse.utility.Overlay;
 import com.foodverse.utility.Router;
+import com.foodverse.utility.core.Button.ButtonSize;
+import com.foodverse.utility.core.Button.ButtonType;
+import com.foodverse.widgets.button.PillButton;
 import com.foodverse.widgets.text.Heading;
-import com.foodverse.widgets.text.Paragraph;
 import com.foodverse.widgets.text.Heading.HeadingSize;
-import com.foodverse.widgets.text.Paragraph.ParagraphSize;
 
 public class ShopOverlay extends Overlay {
 
@@ -16,10 +16,10 @@ public class ShopOverlay extends Overlay {
     public Component getRef() {
         var panel = new JPanel();
         var text = new Heading("ShopOverlay", HeadingSize.L);
-        var buttonText = new Paragraph("Close ShopOverlay", ParagraphSize.M);
-        var button = new Button(buttonText, e -> {
-            Router.closeOverlay();
-        });
+        var button = new PillButton(
+                "Close ShopOverlay ->", ButtonSize.XS, ButtonType.SECONDARY, e -> {
+                    Router.closeOverlay();
+                });
         panel.add(text.getRef());
         panel.add(button.getRef());
         return panel;
--- src/main/java/com/foodverse/pages/HomePage.java
+++ src/main/java/com/foodverse/pages/HomePage.java
@@ -3,13 +3,13 @@ package com.foodverse.pages;
 import java.awt.Component;
 import javax.swing.JPanel;
 import com.foodverse.overlays.ShopOverlay;
-import com.foodverse.utility.Button;
 import com.foodverse.utility.Page;
 import com.foodverse.utility.Router;
+import com.foodverse.utility.core.Button.ButtonSize;
+import com.foodverse.utility.core.Button.ButtonType;
+import com.foodverse.widgets.button.RectButton;
 import com.foodverse.widgets.text.Heading;
-import com.foodverse.widgets.text.Paragraph;
 import com.foodverse.widgets.text.Heading.HeadingSize;
-import com.foodverse.widgets.text.Paragraph.ParagraphSize;
 
 public final class HomePage extends Page {
 
@@ -17,10 +17,10 @@ public final class HomePage extends Page {
     public Component getRef() {
         var panel = new JPanel();
         var text = new Heading("HomePage", HeadingSize.L);
-        var buttonText = new Paragraph("Open ShopOverlay", ParagraphSize.M);
-        var button = new Button(buttonText, e -> {
-            Router.openOverlay(new ShopOverlay());
-        });
+        var button = new RectButton(
+                "Open ShopOverlay ->", ButtonSize.S, ButtonType.PRIMARY, e -> {
+                    Router.openOverlay(new ShopOverlay());
+                });
         panel.add(text.getRef());
         panel.add(button.getRef());
         return panel;

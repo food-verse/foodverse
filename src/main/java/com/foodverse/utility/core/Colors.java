207c93f93578e57462106814b586d0973350ef8b
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-01 18:59:32 +0300

    Move Colors class and add button specific colors

---
+++ src/main/java/com/foodverse/utility/core/Colors.java
@@ -0,0 +1,54 @@
+package com.foodverse.utility.core;
+
+import java.awt.Color;
+
+public final class Colors {
+
+    private Colors() {}
+
+    // Primary
+    public static final Color white = new Color(0xffffffff);
+    public static final Color black = new Color(0xff000000);
+
+    // Secondary
+    public static final Color blue = new Color(0xff276ef1);
+    public static final Color red = new Color(0xffd44333);
+    public static final Color orange = new Color(0xffed6e33);
+    public static final Color yellow = new Color(0xffffc043);
+    public static final Color green = new Color(0xff3aa76d);
+    public static final Color purple = new Color(0xff7356bf);
+    public static final Color brown = new Color(0xff99644c);
+
+    // Gray
+    public static final Color gray50 = new Color(0xfff6f6f6);
+    public static final Color gray100 = new Color(0xffeeeeee);
+    public static final Color gray200 = new Color(0xffe2e2e2);
+    public static final Color gray400 = new Color(0xffafafaf);
+    public static final Color gray500 = new Color(0xff757575);
+    public static final Color gray600 = new Color(0xff545454);
+
+    // Primary Button
+    public static final Color buttonPrimaryFill = new Color(0xff000000);
+    public static final Color buttonPrimaryText = new Color(0xffffffff);
+    public static final Color buttonPrimaryHover = new Color(0xff333333);
+    public static final Color buttonPrimaryActive = new Color(0xff545454);
+
+    // Secondary Button
+    public static final Color buttonSecondaryFill = new Color(0xffeeeeee);
+    public static final Color buttonSecondaryText = new Color(0xff000000);
+    public static final Color buttonSecondaryHover = new Color(0xffe2e2e2);
+    public static final Color buttonSecondaryActive = new Color(0xffcbcbcb);
+
+    // Tertiary Button
+    public static final Color buttonTertiaryFill = new Color(0x00ffffff);
+    public static final Color buttonTertiaryText = new Color(0xff000000);
+    public static final Color buttonTertiaryHover = new Color(0xfff6f6f6);
+    public static final Color buttonTertiaryActive = new Color(0xffeeeeee);
+    public static final Color buttonTertiarySelectedFill = new Color(0xffeeeeee);
+    public static final Color buttonTertiarySelectedText = new Color(0xff000000);
+
+    // Disabled Button
+    public static final Color buttonDisabledFill = new Color(0xfff6f6f6);
+    public static final Color buttonDisabledText = new Color(0xffafafaf);
+
+}

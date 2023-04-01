b262e7114f54775c201b9726b43efc3f73c61a78
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-01 19:05:16 +0300

    Add BaseButton class and embed it in Button

---
+++ src/main/java/com/foodverse/utility/core/BaseButton.java
@@ -0,0 +1,116 @@
+package com.foodverse.utility.core;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+
+final class BaseButton extends JButton {
+
+    private ButtonStyle buttonStyle;
+    private boolean isEnabled;
+
+    public void setStyle(ButtonStyle buttonStyle) {
+        this.buttonStyle = buttonStyle;
+        isEnabled = buttonStyle.isEnabled();
+    }
+
+    public void init() {
+        setPreferredSize(getButtonSize());
+        setMinimumSize(getButtonSize());
+        setMaximumSize(getButtonSize());
+        setFont(new Font(buttonStyle.getTextStyle().getAttributes()));
+        setOpaque(false);
+        setBorder(BorderFactory.createEmptyBorder());
+        addMouseListener(new BaseButtonListener());
+        applyStyle();
+    }
+
+    public void applyStyle() {
+        if (isEnabled) {
+            setForeground(buttonStyle.getDefaultTextColor());
+            setBackground(buttonStyle.getDefaultBackgroundColor());
+        } else {
+            setBackground(buttonStyle.getDisabledBackgroundColor());
+            setForeground(buttonStyle.getDisabledTextColor());
+        }
+    }
+
+    public void toggle() {
+        isEnabled = !isEnabled;
+    }
+
+    private Dimension getButtonSize() {
+        Font font = getFont();
+        FontMetrics fontMetrics = getFontMetrics(font);
+        int textWidth = fontMetrics.stringWidth(getText()) + buttonStyle.getPadding().getLeft()
+                + buttonStyle.getPadding().getRight();
+        int textHeight = fontMetrics.getHeight() + buttonStyle.getPadding().getTop()
+                + buttonStyle.getPadding().getBottom();
+        if (buttonStyle.getMinimumWidth() > textWidth) {
+            textWidth = buttonStyle.getMinimumWidth();
+        }
+        return new Dimension(textWidth, textHeight);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        g.setColor(getBackground());
+        g.fillRoundRect(0, 0, getWidth(), getHeight(),
+                buttonStyle.getBorderRadius(),
+                buttonStyle.getBorderRadius());
+        super.paintComponent(g);
+    }
+
+    @Override
+    protected void paintBorder(Graphics g) {
+        // Skip painting border
+    }
+
+    private class BaseButtonListener extends MouseAdapter {
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            if (isEnabled) {
+                setBackground(buttonStyle.getHoverBackgroundColor());
+                setForeground(buttonStyle.getHoverTextColor());
+            } else {
+                setBackground(buttonStyle.getDisabledBackgroundColor());
+                setForeground(buttonStyle.getDisabledTextColor());
+            }
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            if (isEnabled) {
+                setBackground(buttonStyle.getDefaultBackgroundColor());
+                setForeground(buttonStyle.getDefaultTextColor());
+            } else {
+                setBackground(buttonStyle.getDisabledBackgroundColor());
+                setForeground(buttonStyle.getDisabledTextColor());
+            }
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            if (isEnabled) {
+                setBackground(buttonStyle.getPressBackgroundColor());
+                setForeground(buttonStyle.getPressTextColor());
+            } else {
+                setBackground(buttonStyle.getDisabledBackgroundColor());
+                setForeground(buttonStyle.getDisabledTextColor());
+            }
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            mouseEntered(e);
+        }
+
+    }
+
+}
--- src/main/java/com/foodverse/utility/core/Button.java
+++ src/main/java/com/foodverse/utility/core/Button.java
@@ -4,21 +4,64 @@ import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.util.function.Consumer;
 
-import javax.swing.JButton;
 import com.foodverse.utility.Widget;
 
 public abstract class Button extends Widget {
 
-    private final JButton component = new JButton();
-
-    protected Button(Widget child, Consumer<ActionEvent> onPressed, ButtonStyle buttonStyle) {
-        component.add(child.getRef());
-        component.addActionListener(onPressed::accept);
-    }
+    private final BaseButton component = new BaseButton();
+    private final Consumer<ActionEvent> onPressed;
 
     protected Button(String data, Consumer<ActionEvent> onPressed, ButtonStyle buttonStyle) {
+        this.onPressed = onPressed;
         component.setText(data);
         component.addActionListener(onPressed::accept);
+        component.setStyle(buttonStyle);
+        component.init();
+    }
+
+    public void setText(String data) {
+        component.setText(data);
+        component.applyStyle();
+    }
+
+    public void toggle() {
+        component.toggle();
+        component.applyStyle();
+    }
+
+    protected static ButtonTheme getButtonTheme(ButtonType buttonType) {
+        switch (buttonType) {
+            case PRIMARY:
+                return new ButtonTheme.Builder().build();
+            default:
+            case SECONDARY:
+                return new ButtonTheme.Builder()
+                        .defaultBackgroundColor(Colors.buttonSecondaryFill)
+                        .defaultTextColor(Colors.buttonSecondaryText)
+                        .hoverBackgroundColor(Colors.buttonSecondaryHover)
+                        .hoverTextColor(Colors.buttonSecondaryText)
+                        .pressBackgroundColor(Colors.buttonSecondaryActive)
+                        .pressTextColor(Colors.buttonSecondaryText)
+                        .build();
+            case TERTIARY:
+                return new ButtonTheme.Builder()
+                        .defaultBackgroundColor(Colors.buttonTertiaryFill)
+                        .defaultTextColor(Colors.buttonTertiaryText)
+                        .hoverBackgroundColor(Colors.buttonTertiaryHover)
+                        .hoverTextColor(Colors.buttonTertiaryText)
+                        .pressBackgroundColor(Colors.buttonTertiaryActive)
+                        .pressTextColor(Colors.buttonTertiaryText)
+                        .disabledBackgroundColor(Colors.buttonTertiaryFill)
+                        .build();
+        }
+    }
+
+    public enum ButtonSize {
+        XS, S, M, L;
+    }
+
+    public enum ButtonType {
+        PRIMARY, SECONDARY, TERTIARY;
     }
 
     @Override

69ebb7d26df9d7754cdad13f34bae0157c38721c
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-01 19:03:04 +0300

    Add support for buttons with color

---
+++ src/main/java/com/foodverse/utility/core/ButtonStyle.java
@@ -0,0 +1,131 @@
+package com.foodverse.utility.core;
+
+import java.awt.Color;
+import com.foodverse.utility.EdgeInsets;
+import com.foodverse.widgets.text.Label;
+import com.foodverse.widgets.text.Label.LabelSize;
+
+public final class ButtonStyle {
+
+    private final ButtonTheme buttonTheme;
+    private final TextStyle textStyle;
+    private final EdgeInsets padding;
+    private final int minimumWidth;
+    private final int borderRadius;
+    private final boolean isEnabled;
+
+    public static class Builder {
+
+        private ButtonTheme buttonTheme = new ButtonTheme.Builder()
+                .defaultBackgroundColor(Colors.buttonPrimaryFill)
+                .defaultTextColor(Colors.buttonPrimaryText)
+                .hoverBackgroundColor(Colors.buttonPrimaryHover)
+                .hoverTextColor(Colors.buttonPrimaryText)
+                .pressBackgroundColor(Colors.buttonPrimaryActive)
+                .pressTextColor(Colors.buttonPrimaryText)
+                .disabledBackgroundColor(Colors.buttonDisabledFill)
+                .disabledTextColor(Colors.buttonDisabledText)
+                .build();
+        private TextStyle textStyle = Label.getTextStyle(LabelSize.XS);
+        private EdgeInsets padding = new EdgeInsets.Builder().build();
+        private int minimumWidth = 0;
+        private int borderRadius = 8;
+        private boolean isEnabled = true;
+
+        public Builder buttonTheme(ButtonTheme buttonTheme) {
+            this.buttonTheme = buttonTheme;
+            return this;
+        }
+
+        public Builder textStyle(TextStyle textStyle) {
+            this.textStyle = textStyle;
+            return this;
+        }
+
+        public Builder padding(EdgeInsets padding) {
+            this.padding = padding;
+            return this;
+        }
+
+        public Builder minimumWidth(int minimumWidth) {
+            this.minimumWidth = minimumWidth;
+            return this;
+        }
+
+        public Builder borderRadius(int borderRadius) {
+            this.borderRadius = borderRadius;
+            return this;
+        }
+
+        public Builder isEnabled(boolean isEnabled) {
+            this.isEnabled = isEnabled;
+            return this;
+        }
+
+        public ButtonStyle build() {
+            return new ButtonStyle(this);
+        }
+    }
+
+    private ButtonStyle(Builder builder) {
+        this.buttonTheme = builder.buttonTheme;
+        this.textStyle = builder.textStyle;
+        this.padding = builder.padding;
+        this.minimumWidth = builder.minimumWidth;
+        this.borderRadius = builder.borderRadius;
+        this.isEnabled = builder.isEnabled;
+    }
+
+    public Color getDefaultBackgroundColor() {
+        return buttonTheme.getDefaultBackgroundColor();
+    }
+
+    public Color getDefaultTextColor() {
+        return buttonTheme.getDefaultTextColor();
+    }
+
+    public Color getHoverBackgroundColor() {
+        return buttonTheme.getHoverBackgroundColor();
+    }
+
+    public Color getHoverTextColor() {
+        return buttonTheme.getHoverTextColor();
+    }
+
+    public Color getPressBackgroundColor() {
+        return buttonTheme.getPressBackgroundColor();
+    }
+
+    public Color getPressTextColor() {
+        return buttonTheme.getPressTextColor();
+    }
+
+    public Color getDisabledBackgroundColor() {
+        return buttonTheme.getDisabledBackgroundColor();
+    }
+
+    public Color getDisabledTextColor() {
+        return buttonTheme.getDisabledTextColor();
+    }
+
+    public TextStyle getTextStyle() {
+        return textStyle;
+    }
+
+    public EdgeInsets getPadding() {
+        return padding;
+    }
+
+    public int getMinimumWidth() {
+        return minimumWidth;
+    }
+
+    public int getBorderRadius() {
+        return borderRadius;
+    }
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+}
---
+++ src/main/java/com/foodverse/utility/core/ButtonTheme.java
@@ -0,0 +1,115 @@
+package com.foodverse.utility.core;
+
+import java.awt.Color;
+
+public class ButtonTheme {
+
+    private final Color defaultBackgroundColor;
+    private final Color defaultTextColor;
+    private final Color hoverBackgroundColor;
+    private final Color hoverTextColor;
+    private final Color pressBackgroundColor;
+    private final Color pressTextColor;
+    private final Color disabledBackgroundColor;
+    private final Color disabledTextColor;
+
+    public static class Builder {
+
+        private Color defaultBackgroundColor = Colors.buttonPrimaryFill;
+        private Color defaultTextColor = Colors.buttonPrimaryText;
+        private Color hoverBackgroundColor = Colors.buttonPrimaryHover;
+        private Color hoverTextColor = Colors.buttonPrimaryText;
+        private Color pressBackgroundColor = Colors.buttonPrimaryActive;
+        private Color pressTextColor = Colors.buttonPrimaryText;
+        private Color disabledBackgroundColor = Colors.buttonDisabledFill;
+        private Color disabledTextColor = Colors.buttonDisabledText;
+
+        public Builder defaultBackgroundColor(Color color) {
+            this.defaultBackgroundColor = color;
+            return this;
+        }
+
+        public Builder defaultTextColor(Color color) {
+            this.defaultTextColor = color;
+            return this;
+        }
+
+        public Builder hoverBackgroundColor(Color color) {
+            this.hoverBackgroundColor = color;
+            return this;
+        }
+
+        public Builder hoverTextColor(Color color) {
+            this.hoverTextColor = color;
+            return this;
+        }
+
+        public Builder pressBackgroundColor(Color color) {
+            this.pressBackgroundColor = color;
+            return this;
+        }
+
+        public Builder pressTextColor(Color color) {
+            this.pressTextColor = color;
+            return this;
+        }
+
+        public Builder disabledBackgroundColor(Color color) {
+            this.disabledBackgroundColor = color;
+            return this;
+        }
+
+        public Builder disabledTextColor(Color color) {
+            this.disabledTextColor = color;
+            return this;
+        }
+
+        public ButtonTheme build() {
+            return new ButtonTheme(this);
+        }
+    }
+
+    private ButtonTheme(Builder builder) {
+        this.defaultBackgroundColor = builder.defaultBackgroundColor;
+        this.defaultTextColor = builder.defaultTextColor;
+        this.hoverBackgroundColor = builder.hoverBackgroundColor;
+        this.hoverTextColor = builder.hoverTextColor;
+        this.pressBackgroundColor = builder.pressBackgroundColor;
+        this.pressTextColor = builder.pressTextColor;
+        this.disabledBackgroundColor = builder.disabledBackgroundColor;
+        this.disabledTextColor = builder.disabledTextColor;
+    }
+
+    public Color getDefaultBackgroundColor() {
+        return defaultBackgroundColor;
+    }
+
+    public Color getDefaultTextColor() {
+        return defaultTextColor;
+    }
+
+    public Color getHoverBackgroundColor() {
+        return hoverBackgroundColor;
+    }
+
+    public Color getHoverTextColor() {
+        return hoverTextColor;
+    }
+
+    public Color getPressBackgroundColor() {
+        return pressBackgroundColor;
+    }
+
+    public Color getPressTextColor() {
+        return pressTextColor;
+    }
+
+    public Color getDisabledBackgroundColor() {
+        return disabledBackgroundColor;
+    }
+
+    public Color getDisabledTextColor() {
+        return disabledTextColor;
+    }
+
+}

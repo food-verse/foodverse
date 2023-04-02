3d2a1c512da9e05686cc14187c333bcd0c5d5b76
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-02 15:12:57 +0300

    Add support for exporting Shell Options

--- src/main/java/com/foodverse/utility/Shell.java
+++ src/main/java/com/foodverse/utility/Shell.java
@@ -1,12 +1,12 @@
 package com.foodverse.utility;
 
-import java.awt.Color;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
 public final class Shell {
 
     private static Shell shell;
+    private static ShellOptions options;
 
     // The underlying frame holding our widgets
     private static final JFrame mainFrame = new JFrame();
@@ -21,12 +21,17 @@ public final class Shell {
     }
 
     public static void init(ShellOptions options) {
+        Shell.options = options;
         AssetManager.loadFont("Inter");
         AssetManager.loadFont("IBMPlexMono");
         mainFrame.setSize(options.getDimension());
-        mainFrame.getContentPane().setBackground(Color.white);
+        mainFrame.getContentPane().setBackground(options.getBackgroundColor());
         mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         mainFrame.setVisible(true);
+    }
+
+    public static ShellOptions getOptions() {
+        return options;
     }
 
     public JFrame getMainFrame() {

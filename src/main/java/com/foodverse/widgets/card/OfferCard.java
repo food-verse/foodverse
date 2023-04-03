5ae2c79d7212affdf1dbbcf26e352e4408a23274
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-03 20:52:39 +0300

    Add Card classes

---
+++ src/main/java/com/foodverse/widgets/card/OfferCard.java
@@ -0,0 +1,14 @@
+package com.foodverse.widgets.card;
+
+import java.awt.Component;
+import javax.swing.JPanel;
+import com.foodverse.utility.Widget;
+
+public final class OfferCard extends Widget {
+
+    @Override
+    public Component getRef() {
+        return new JPanel();
+    }
+
+}
---
+++ src/main/java/com/foodverse/widgets/card/ShopCard.java
@@ -0,0 +1,14 @@
+package com.foodverse.widgets.card;
+
+import java.awt.Component;
+import javax.swing.JPanel;
+import com.foodverse.utility.Widget;
+
+public final class ShopCard extends Widget {
+
+    @Override
+    public Component getRef() {
+        return new JPanel();
+    }
+
+}

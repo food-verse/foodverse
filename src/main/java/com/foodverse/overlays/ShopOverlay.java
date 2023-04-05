d51458bafeb91c135b6d2f200128a06d89d711ef
SofiaKal <128231824+SofiaKal@users.noreply.github.com>
2023-04-05 19:10:56 +0300

    store

--- src/main/java/com/foodverse/overlays/ShopOverlay.java
+++ src/main/java/com/foodverse/overlays/ShopOverlay.java
@@ -34,13 +34,32 @@ public class ShopOverlay extends Overlay {
 
         var panel1 = new JPanel();
         var panel2 = new JPanel();
-        var productAddButton = new PillButton("Add", ButtonSize.XS, ButtonType.SECONDARY, e -> {
-        });
+        var panel3 = new JPanel();
+
+
+
+        var rateLabel = new JLabel("Rate ");
+        rateLabel.setFont(new Font("Courier New", Font.BOLD, 25));
+
+
+        var rate1 = new JRadioButton("1 ");
+        var rate2 = new JRadioButton("2 ");
+        var rate3 = new JRadioButton("3 ");
+        var rate4 = new JRadioButton("4 ");
+        var rate5 = new JRadioButton("5 ");
+
+
+        var group = new ButtonGroup();
+        group.add(rate1);
+        group.add(rate2);
+        group.add(rate3);
+        group.add(rate4);
+        group.add(rate5);
 
 
         var offers = new JLabel("Offers");
         var menu = new JLabel("Menu");
-        var menuItem = new JLabel("Item");
+
 
 
         offers.setFont(new Font("Courier New", Font.BOLD, 25));
@@ -49,20 +68,37 @@ public class ShopOverlay extends Overlay {
         menu.setForeground(Color.white);
 
 
-        panel1.setPreferredSize(new Dimension(1200, 300));
+        panel1.setPreferredSize(new Dimension(1300, 300));
         panel1.setBackground(Color.lightGray);
         panel1.add(offers);
-        panel1.add(menuItem);
-        panel1.add(productAddButton.getRef());
 
 
-        panel2.setPreferredSize(new Dimension(1200, 300));
+        panel2.setPreferredSize(new Dimension(1300, 300));
         panel2.setBackground(Color.lightGray);
         panel2.add(menu);
 
 
-        panel.add(panel1);
-        panel.add(panel2);
+        panel3.add(rateLabel);
+        panel3.add(rate1);
+        panel3.add(rate2);
+        panel3.add(rate3);
+        panel3.add(rate4);
+        panel3.add(rate5);
+
+
+        for (var i = 0; i < 3; i++) {
+            var productAddButton = new PillButton("Add", ButtonSize.XS, ButtonType.SECONDARY, e -> {
+            });
+            var panel4 = new JPanel();
+            var menuItem = new JLabel("Item");
+
+            panel4.setPreferredSize(new Dimension(1200, 60));
+            panel4.add(menuItem);
+            panel4.add(productAddButton.getRef());
+            panel1.add(panel4);
+
+        }
+
 
 
         JScrollPane scroll1 = new JScrollPane(panel1, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
@@ -74,7 +110,14 @@ public class ShopOverlay extends Overlay {
         panel.add(scroll2);
 
 
+        panel.add(panel3);
+
+
         return panel;
+    }
+
+    public void showItems() {
+
     }
 
     // public void paint(Graphics g)

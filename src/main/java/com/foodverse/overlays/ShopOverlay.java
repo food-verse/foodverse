f76e118ac0bd68737e884d55dbafab6922442029
SofiaKal <128231824+SofiaKal@users.noreply.github.com>
2023-04-05 18:07:06 +0300

    shop

--- src/main/java/com/foodverse/overlays/ShopOverlay.java
+++ src/main/java/com/foodverse/overlays/ShopOverlay.java
@@ -13,7 +13,6 @@ import java.awt.Color;
 
 import java.awt.Dimension;
 import java.awt.Font;
-// import java.awt.Frame;
 // import java.awt.Graphics;
 import javax.swing.*;
 
@@ -32,54 +31,47 @@ public class ShopOverlay extends Overlay {
         panel.add(button.getRef());
         panel.setOpaque(false);
 
-        var panel0 = new JPanel();
+
         var panel1 = new JPanel();
         var panel2 = new JPanel();
+        var productAddButton = new PillButton("Add", ButtonSize.XS, ButtonType.SECONDARY, e -> {
+        });
+
 
         var offers = new JLabel("Offers");
         var menu = new JLabel("Menu");
-        // var menuItem = new JLabel("Item");
-        // var offerProducts = new JTextArea("12345");
+        var menuItem = new JLabel("Item");
+
 
         offers.setFont(new Font("Courier New", Font.BOLD, 25));
         menu.setFont(new Font("Courier New", Font.BOLD, 25));
         offers.setForeground(Color.white);
         menu.setForeground(Color.white);
 
-        panel1.setPreferredSize(new Dimension(600, 190));
-        panel1.setBackground(Color.gray);
+
+        panel1.setPreferredSize(new Dimension(1200, 300));
+        panel1.setBackground(Color.lightGray);
         panel1.add(offers);
-        // panel1.add(menuItem);
-        // panel1.add(offerProducts);
-        // panel1.add (new LinePanel());
+        panel1.add(menuItem);
+        panel1.add(productAddButton.getRef());
 
-        panel2.setPreferredSize(new Dimension(600, 190));
-        panel2.setBackground(Color.gray);
 
+        panel2.setPreferredSize(new Dimension(1200, 300));
+        panel2.setBackground(Color.lightGray);
         panel2.add(menu);
-        // panel2.add(menuProducts);
 
 
-        // LinePanel linePanel = new LinePanel();
-        // linePanel.setPreferredSize(new Dimension(panel1.getWidth(), 1));
-        // panel1.add(linePanel);
-
-
-        panel0.setBackground(Color.lightGray);
-        panel0.add(panel1);
-        panel0.add(panel2);
-
+        panel.add(panel1);
+        panel.add(panel2);
 
 
         JScrollPane scroll1 = new JScrollPane(panel1, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
                 JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        panel0.add(scroll1);
+        panel.add(scroll1);
 
         JScrollPane scroll2 = new JScrollPane(panel2, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
                 JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        panel0.add(scroll2);
-
-        panel.add(panel0);
+        panel.add(scroll2);
 
 
         return panel;

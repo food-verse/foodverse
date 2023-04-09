ca59c450e868774a8058084915d84279f615a952
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-09 19:42:57 +0300

    Fix imports

--- src/main/java/com/foodverse/widgets/media/Image.java
+++ src/main/java/com/foodverse/widgets/media/Image.java
@@ -3,10 +3,10 @@ package com.foodverse.widgets.media;
 import java.awt.Component;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
-import com.foodverse.utility.AssetManager;
 import com.foodverse.utility.Widget;
-import com.foodverse.utility.core.ImageAsset;
-import com.foodverse.utility.core.ImageStyle;
+import com.foodverse.utility.core.ui.ImageAsset;
+import com.foodverse.utility.core.ui.ImageStyle;
+import com.foodverse.utility.system.AssetManager;
 
 public final class Image extends Widget {
 
--- src/main/java/com/foodverse/widgets/media/NetworkImage.java
+++ src/main/java/com/foodverse/widgets/media/NetworkImage.java
@@ -3,9 +3,9 @@ package com.foodverse.widgets.media;
 import java.awt.Component;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
-import com.foodverse.utility.AssetManager;
 import com.foodverse.utility.Widget;
-import com.foodverse.utility.core.ImageStyle;
+import com.foodverse.utility.core.ui.ImageStyle;
+import com.foodverse.utility.system.AssetManager;
 
 public final class NetworkImage extends Widget {
 

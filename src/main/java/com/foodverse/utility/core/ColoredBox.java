ff839dc581f22abfdd5ec4917201944e23ea563c
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-09 11:57:39 +0300

    Add ColoredBox class

---
+++ src/main/java/com/foodverse/utility/core/ColoredBox.java
@@ -0,0 +1,45 @@
+package com.foodverse.utility.core;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridBagLayout;
+import java.awt.RenderingHints;
+import javax.swing.JPanel;
+import com.foodverse.utility.Widget;
+
+public final class ColoredBox extends JPanel {
+
+    private final ColoredBoxStyle style;
+
+    public ColoredBox(Widget child) {
+        this(new ColoredBoxStyle.Builder().build(), child);
+    }
+
+    public ColoredBox(ColoredBoxStyle style, Widget child) {
+        super(new GridBagLayout());
+        setForeground(style.getBorderColor());
+        setBackground(style.getFillColor());
+        setOpaque(false);
+        add(child.getRef());
+        setMaximumSize(getMinimumSize());
+        this.style = style;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Dimension arcs = new Dimension(style.getBorderRadius(), style.getBorderRadius());
+        int width = getWidth();
+        int height = getHeight();
+        Graphics2D graphics = (Graphics2D) g;
+        graphics.setRenderingHint(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        graphics.setColor(style.getFillColor());
+        graphics.fillRoundRect(0, 0, width - 1, height - 1, arcs.width, arcs.height);
+        graphics.setColor(style.getBorderColor());
+        graphics.drawRoundRect(0, 0, width - 1, height - 1, arcs.width, arcs.height);
+    }
+
+}
---
+++ src/main/java/com/foodverse/utility/core/ColoredBoxStyle.java
@@ -0,0 +1,56 @@
+package com.foodverse.utility.core;
+
+import java.awt.Color;
+
+public final class ColoredBoxStyle {
+
+    private final Color borderColor;
+    private final Color fillColor;
+    private final int borderRadius;
+
+    public static class Builder {
+
+        private Color borderColor = Colors.gray200;
+        private Color fillColor = Colors.white;
+        private int borderRadius = 16;
+
+        public Builder borderColor(Color color) {
+            this.borderColor = color;
+            return this;
+        }
+
+        public Builder fillColor(Color color) {
+            this.fillColor = color;
+            return this;
+        }
+
+        public Builder borderRadius(int borderRadius) {
+            this.borderRadius = borderRadius;
+            return this;
+        }
+
+        public ColoredBoxStyle build() {
+            return new ColoredBoxStyle(this);
+        }
+
+    }
+
+    private ColoredBoxStyle(Builder builder) {
+        this.borderColor = builder.borderColor;
+        this.fillColor = builder.fillColor;
+        this.borderRadius = builder.borderRadius;
+    }
+
+    public Color getBorderColor() {
+        return borderColor;
+    }
+
+    public Color getFillColor() {
+        return fillColor;
+    }
+
+    public int getBorderRadius() {
+        return borderRadius;
+    }
+
+}

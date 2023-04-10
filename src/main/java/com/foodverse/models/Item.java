78fde6a0fe2bda88561c159e46d901b78fdf6af9
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-10 14:34:49 +0300

    Update item class to include price

--- src/main/java/com/foodverse/models/Item.java
+++ src/main/java/com/foodverse/models/Item.java
@@ -1,15 +1,34 @@
 package com.foodverse.models;
 
+import com.google.gson.annotations.SerializedName;
+
 public final class Item {
 
-    private final String name;
+    @SerializedName("name")
+    private String name;
 
-    public Item(String name) {
+    @SerializedName("price")
+    private float price;
+
+    public Item(String name, float price) {
         this.name = name;
+        this.price = price;
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
     }
 
 }

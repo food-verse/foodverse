87c86c08861ee16a202b4ce6592fca2b8f04aeee
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-11 00:35:14 +0300

    Fix formatting

--- src/main/java/com/foodverse/utility/core/layout/Align.java
+++ src/main/java/com/foodverse/utility/core/layout/Align.java
@@ -3,13 +3,15 @@ package com.foodverse.utility.core.layout;
 import java.awt.GridBagConstraints;
 
 public enum Align {
-    CENTER(GridBagConstraints.CENTER), PAGE_START(GridBagConstraints.PAGE_START), PAGE_END(
-            GridBagConstraints.PAGE_END), LINE_START(GridBagConstraints.LINE_START), LINE_END(
-                    GridBagConstraints.LINE_END), FIRST_LINE_START(
-                            GridBagConstraints.FIRST_LINE_START), FIRST_LINE_END(
-                                    GridBagConstraints.FIRST_LINE_END), LAST_LINE_END(
-                                            GridBagConstraints.LAST_LINE_END), LAST_LINE_START(
-                                                    GridBagConstraints.LAST_LINE_START);
+    CENTER(GridBagConstraints.CENTER),
+    PAGE_START(GridBagConstraints.PAGE_START),
+    PAGE_END(GridBagConstraints.PAGE_END),
+    LINE_START(GridBagConstraints.LINE_START),
+    LINE_END(GridBagConstraints.LINE_END),
+    FIRST_LINE_START(GridBagConstraints.FIRST_LINE_START),
+    FIRST_LINE_END(GridBagConstraints.FIRST_LINE_END),
+    LAST_LINE_END(GridBagConstraints.LAST_LINE_END),
+    LAST_LINE_START(GridBagConstraints.LAST_LINE_START);
 
     private final int anchor;
 

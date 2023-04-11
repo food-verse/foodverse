28eaeeaf42fd0bfea01a078d0b5626efcd1431da
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-11 14:02:56 +0300

    Update FileManager to support shop loading

--- src/main/java/com/foodverse/utility/system/FileManager.java
+++ src/main/java/com/foodverse/utility/system/FileManager.java
@@ -1,30 +1,37 @@
 package com.foodverse.utility.system;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import com.foodverse.models.Shop;
 import com.foodverse.models.UserCredentials;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public final class FileManager {
 
     private static final Logger logger = Logger.getLogger(FileManager.class.getName());
 
+    private static final Gson gson = new Gson();
+
     private FileManager() {}
 
     public static List<UserCredentials> loadDatabase() {
-        String fileName = Files.DATABASE.getFileName();
         List<UserCredentials> loadedUsers = new ArrayList<>();
-        File file = new File(fileName);
+        File file = Files.DATABASE.getFile();
         if (file.exists()) {
             try {
                 FileInputStream fileInputStream = new FileInputStream(file);
@@ -38,35 +45,69 @@ public final class FileManager {
                 fileInputStream.close();
                 objectInputStream.close();
             } catch (IOException | ClassNotFoundException e) {
-                logger.log(Level.INFO, "Could not load \"{0}\" image.", fileName);
+                logger.log(Level.INFO, "Could not load database.");
             }
         } else {
-            logger.log(Level.INFO, "There is no such file: {0}", fileName);
+            logger.log(Level.INFO, "There is no such file: {0}", file.getName());
         }
         return loadedUsers;
     }
 
     public static void saveDatabase(List<UserCredentials> users) {
-        String fileName = Files.DATABASE.getFileName();
-        File file = new File(fileName);
-        if (file.exists()) {
+        File file = Files.DATABASE.getFile();
+        if (!file.exists()) {
             try {
-                FileOutputStream outputStream = new FileOutputStream(file);
-                ObjectOutputStream stream = new ObjectOutputStream(outputStream);
-                stream.writeObject(users);
-                outputStream.close();
-                stream.close();
+                file.getParentFile().mkdirs();
+                file.createNewFile();
             } catch (IOException e) {
-                logger.log(Level.INFO, "Could not save user credentials to the database.");
+                logger.log(Level.INFO, "Could not create a file for saving the list of users.");
+            }
+        }
+        try {
+            FileOutputStream outputStream = new FileOutputStream(file);
+            ObjectOutputStream stream = new ObjectOutputStream(outputStream);
+            stream.writeObject(users);
+            outputStream.close();
+            stream.close();
+        } catch (IOException e) {
+            logger.log(Level.INFO, "Could not save user credentials to the database.");
+        }
+    }
+
+    public static List<Shop> loadShops() {
+        File file = Files.SHOPS.getFile();
+        if (file.exists()) {
+            Type shopListType = new TypeToken<List<Shop>>() {}.getType();
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                return gson.fromJson(reader, shopListType);
+            } catch (IOException e) {
+                logger.log(Level.INFO, "Could not load the list of shops.");
             }
         } else {
-            logger.log(Level.INFO, "There is no such file: {0}", fileName);
+            logger.log(Level.INFO, "There is no such file: {0}", file.getName());
+        }
+        return List.of();
+    }
+
+    public static void saveShops(List<Shop> shops) {
+        File file = Files.SHOPS.getFile();
+        if (!file.exists()) {
+            try {
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+            } catch (IOException e) {
+                logger.log(Level.INFO, "Could not create a file for saving the list of shops.");
+            }
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            gson.toJson(shops, writer);
+        } catch (IOException e) {
+            logger.log(Level.INFO, "Could not save the list of shops.");
         }
     }
 
     public static Optional<String> getRandomRecoveryQuestion(int questionIndex) {
-        String fileName = Files.QUESTIONS.getFileName();
-        File file = new File(fileName);
+        File file = Files.QUESTIONS.getFile();
         String question = null;
         int index = 1;
         if (file.exists()) {
@@ -83,10 +124,10 @@ public final class FileManager {
                 bufferedReader.close();
                 fileReader.close();
             } catch (IOException e) {
-                logger.log(Level.INFO, "Could not load questions from: {0}", fileName);
+                logger.log(Level.INFO, "Could not load questions from: {0}", file.getName());
             }
         } else {
-            logger.log(Level.INFO, "There is no such file: {0}", fileName);
+            logger.log(Level.INFO, "There is no such file: {0}", file.getName());
         }
         return Optional.ofNullable(question);
     }

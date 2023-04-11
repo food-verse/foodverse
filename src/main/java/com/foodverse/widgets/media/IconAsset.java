e57dc381d7af6c73ac80f65c4691b75151b82087
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-11 13:24:51 +0300

    Remove ImageAsset and add IconAsset

---
+++ src/main/java/com/foodverse/widgets/media/IconAsset.java
@@ -0,0 +1,22 @@
+package com.foodverse.widgets.media;
+
+import java.io.File;
+
+public enum IconAsset {
+    BRAND("brand.png"), STAR("rating_star.png"), AVATAR("avatar.png");
+
+    private final String fileName;
+
+    IconAsset(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public File getFile() {
+        return new File(String.format("assets/icons/%s", fileName));
+    }
+
+}

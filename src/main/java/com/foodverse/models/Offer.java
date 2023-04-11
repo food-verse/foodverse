c94962b9eab8c3f9369c2a63e023ce2b4e58bb91
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-11 13:57:36 +0300

    Cleanup

--- src/main/java/com/foodverse/models/Offer.java
+++ src/main/java/com/foodverse/models/Offer.java
@@ -6,23 +6,21 @@ import com.google.gson.annotations.SerializedName;
 public final class Offer {
 
     @SerializedName("items")
-    private Map<Item, Integer> items;
+    private Map<String, Integer> items;
 
     @SerializedName("total")
     private float total;
 
-    public Offer() {}
-
-    public Offer(Map<Item, Integer> items, float total) {
+    public Offer(Map<String, Integer> items, float total) {
         this.items = items;
         this.total = total;
     }
 
-    public Map<Item, Integer> getItems() {
+    public Map<String, Integer> getItems() {
         return items;
     }
 
-    public void setItems(Map<Item, Integer> items) {
+    public void setItems(Map<String, Integer> items) {
         this.items = items;
     }
 

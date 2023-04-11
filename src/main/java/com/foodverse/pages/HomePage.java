ad4df1a3b3671bfc391780f519ad17ee7c54d15d
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-11 14:03:53 +0300

    Remove mock data creation from HomePage

--- src/main/java/com/foodverse/pages/HomePage.java
+++ src/main/java/com/foodverse/pages/HomePage.java
@@ -2,14 +2,15 @@ package com.foodverse.pages;
 
 import java.awt.Component;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 import com.foodverse.models.Item;
-import com.foodverse.models.Offer;
+import com.foodverse.models.Shop;
 import com.foodverse.utility.Widget;
 import com.foodverse.utility.core.layout.Align;
 import com.foodverse.utility.core.layout.EdgeInsets;
@@ -19,7 +20,7 @@ import com.foodverse.utility.core.ui.Button.ButtonType;
 import com.foodverse.utility.navigation.Page;
 import com.foodverse.utility.navigation.Pages;
 import com.foodverse.utility.navigation.Router;
-import com.foodverse.utility.system.ImageAsset;
+import com.foodverse.utility.system.FileManager;
 import com.foodverse.widgets.button.RectButton;
 import com.foodverse.widgets.card.ShopProps;
 import com.foodverse.widgets.layout.Carousel;
@@ -27,6 +28,7 @@ import com.foodverse.widgets.layout.Column;
 import com.foodverse.widgets.layout.ListTile;
 import com.foodverse.widgets.layout.Row;
 import com.foodverse.widgets.layout.ScrollView;
+import com.foodverse.widgets.media.IconAsset;
 import com.foodverse.widgets.media.Image;
 import com.foodverse.widgets.card.OfferProps;
 import com.foodverse.widgets.card.OrderProps;
@@ -55,11 +57,11 @@ public final class HomePage extends Page {
         headingRow.setOpaque(false);
         headingRow.setLayout(new BoxLayout(headingRow, BoxLayout.X_AXIS));
         // Creating image widgets...
-        var brandImage = new Image(ImageAsset.BRAND, new ImageStyle.Builder()
+        var brandImage = new Image(IconAsset.BRAND, new ImageStyle.Builder()
                 .width(328)
                 .height(72)
                 .build());
-        var avatarImage = new Image(ImageAsset.AVATAR, new ImageStyle.Builder()
+        var avatarImage = new Image(IconAsset.AVATAR, new ImageStyle.Builder()
                 .width(48)
                 .height(48)
                 .build());
@@ -84,53 +86,36 @@ public final class HomePage extends Page {
         var nearbyTile = new ListTile("Nearby");
         // Add the heading for the shops' carousel to the main panel
         panel.add(nearbyTile.getRef());
-        // TODO: Remove list of ShopCard props
-        List<ShopProps> shopProps = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            shopProps.add(new ShopProps.Builder()
-                    .thumbnail(ImageAsset.BURGER)
-                    .name("Burgerlicious")
-                    .rating(5f)
-                    .minOrder(7f)
-                    .prepTime(30)
-                    .build());
-        }
+
+        // Loading list of shops...
+        // TODO: Remove dependency to FileManager
+        List<Shop> shops = FileManager.loadShops();
+
+        // Turning Shop list into ShopProp list...
+        List<ShopProps> shopProps = shops.stream()
+                .map(ShopProps::from)
+                .sorted(Comparator.comparingDouble(ShopProps::getRating).reversed())
+                .collect(Collectors.toList());
+
         // Create a carousel for the nearby shops
         var shopCarousel = new Carousel(shopProps);
+
         // Add the carousel for the nearby shops to the main panel
         panel.add(shopCarousel.getRef());
+
         // Heading for the carousel of offers
         var offersTile = new ListTile("Offers");
+
         // Add the heading for the offers' carousel to the main panel
         panel.add(offersTile.getRef());
-        // TODO: Remove map of offers when the process of loading the offers is ready
-        // Creating first offer...
-        Map<Item, Integer> firstOfferItems = new HashMap<>();
-        firstOfferItems.put(new Item("Pizza Margarita", 3.20f), 1);
-        firstOfferItems.put(new Item("Coca Cola", 1.30f), 2);
-        Offer firstOffer = new Offer(firstOfferItems, 5.20f);
-        // Creating second offer...
-        Map<Item, Integer> secondOfferItems = new HashMap<>();
-        secondOfferItems.put(new Item("Pizza Picolla", 4.30f), 1);
-        secondOfferItems.put(new Item("Coca Cola", 1.30f), 1);
-        Offer secondOffer = new Offer(secondOfferItems, 3.40f);
-        // Creating third offer...
-        Map<Item, Integer> thirdOfferItems = new HashMap<>();
-        thirdOfferItems.put(new Item("Pizza Al Forno", 2.90f), 1);
-        thirdOfferItems.put(new Item("Fanta", 1.30f), 2);
-        Offer thirdOffer = new Offer(firstOfferItems, 8.60f);
-        // Creating the array of offers...
-        Offer[] offerItems = {firstOffer, secondOffer, thirdOffer};
-        // TODO: Remove list of OfferCard props
-        List<OfferProps> offerProps = new ArrayList<>();
-        for (int i = 0; i < 2; i++) {
-            offerProps.add(new OfferProps.Builder()
-                    .thumbnail(ImageAsset.SMALL_BURGER)
-                    .name("Burgerlicious")
-                    .rating(5.0f)
-                    .offers(offerItems)
-                    .build());
-        }
+
+        // Turning Shop list into ShopProp list...
+        List<OfferProps> offerProps = shops.stream()
+                .filter(shop -> !shop.getOffers().isEmpty())
+                .map(OfferProps::from)
+                .sorted(Comparator.comparingDouble(OfferProps::getRating).reversed())
+                .collect(Collectors.toList());
+
         // Create a carousel for the available offers
         var offerCarousel = new Carousel(offerProps);
         // Add the carousel for the available offers to the main panel
@@ -148,7 +133,7 @@ public final class HomePage extends Page {
         List<OrderProps> orderProps = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             orderProps.add(new OrderProps.Builder()
-                    .thumbnail(ImageAsset.BURGER)
+                    .thumbnail("burger-medium.png")
                     .name("Burgerlicious")
                     .rating(5f)
                     .items(orderItems)

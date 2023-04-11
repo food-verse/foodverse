a6b3b80f2bbcbeca92e369362f9923f34906b8d8
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-11 13:25:15 +0300

    Add new Image constructor

--- src/main/java/com/foodverse/widgets/media/Image.java
+++ src/main/java/com/foodverse/widgets/media/Image.java
@@ -1,18 +1,26 @@
 package com.foodverse.widgets.media;
 
 import java.awt.Component;
+import java.io.File;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import com.foodverse.utility.Widget;
 import com.foodverse.utility.core.ui.ImageStyle;
 import com.foodverse.utility.system.AssetManager;
-import com.foodverse.utility.system.ImageAsset;
 
 public final class Image extends Widget {
 
     private final JLabel component = new JLabel();
 
-    public Image(ImageAsset asset, ImageStyle imageStyle) {
+    public Image(String assetName, ImageStyle imageStyle) {
+        component.setPreferredSize(imageStyle.getDimension());
+        File file = new File(String.format("assets/images/%s", assetName));
+        AssetManager
+                .getImage(file, imageStyle.getWidth(), imageStyle.getHeight())
+                .ifPresent(bufferedImage -> component.setIcon(new ImageIcon(bufferedImage)));
+    }
+
+    public Image(IconAsset asset, ImageStyle imageStyle) {
         component.setPreferredSize(imageStyle.getDimension());
         AssetManager
                 .getImage(asset.getFile(), imageStyle.getWidth(), imageStyle.getHeight())

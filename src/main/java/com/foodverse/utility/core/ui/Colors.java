f7dbab6e16b52b49d02a0df957656ddb7850b00f
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-11 18:44:21 +0300

    Update color palette

--- src/main/java/com/foodverse/utility/core/ui/Colors.java
+++ src/main/java/com/foodverse/utility/core/ui/Colors.java
@@ -10,45 +10,117 @@ public final class Colors {
     public static final Color white = new Color(0xffffffff);
     public static final Color black = new Color(0xff000000);
 
-    // Secondary
-    public static final Color blue = new Color(0xff276ef1);
-    public static final Color red = new Color(0xffd44333);
-    public static final Color orange = new Color(0xffed6e33);
-    public static final Color yellow = new Color(0xffffc043);
-    public static final Color green = new Color(0xff3aa76d);
-    public static final Color purple = new Color(0xff7356bf);
-    public static final Color brown = new Color(0xff99644c);
+    // Red
+    public static final Color red50 = new Color(0xfffdf0ef);
+    public static final Color red100 = new Color(0xfffadbd7);
+    public static final Color red200 = new Color(0xfff4afa7);
+    public static final Color red300 = new Color(0xffeb7567);
+    public static final Color red400 = new Color(0xffd44333);
+    public static final Color red500 = new Color(0xffae372a);
+    public static final Color red600 = new Color(0xff892c21);
+    public static final Color red700 = new Color(0xff5c1d16);
+
+    // Orange
+    public static final Color orange50 = new Color(0xfffef3ef);
+    public static final Color orange100 = new Color(0xfffbe2d6);
+    public static final Color orange200 = new Color(0xfff7bfa5);
+    public static final Color orange300 = new Color(0xfff19164);
+    public static final Color orange400 = new Color(0xffed6e33);
+    public static final Color orange500 = new Color(0xffb45427);
+    public static final Color orange600 = new Color(0xff8e421f);
+    public static final Color orange700 = new Color(0xff5f2c14);
+
+    // Green
+    public static final Color green50 = new Color(0xfff0f9f4);
+    public static final Color green100 = new Color(0xffdaf0e3);
+    public static final Color green200 = new Color(0xffaeddc2);
+    public static final Color green300 = new Color(0xff73c496);
+    public static final Color green400 = new Color(0xff3aa76d);
+    public static final Color green500 = new Color(0xff368759);
+    public static final Color green600 = new Color(0xff2b6b46);
+    public static final Color green700 = new Color(0xff1c472f);
+
+    // Blue
+    public static final Color blue50 = new Color(0xffeef3fe);
+    public static final Color blue100 = new Color(0xffd4e2fc);
+    public static final Color blue200 = new Color(0xffa0bff9);
+    public static final Color blue300 = new Color(0xff5b91f4);
+    public static final Color blue400 = new Color(0xff276ef1);
+    public static final Color blue500 = new Color(0xff1e54b7);
+    public static final Color blue600 = new Color(0xff174291);
+    public static final Color blue700 = new Color(0xff102c60);
+
+    // Purple
+    public static final Color purple50 = new Color(0xfff4f1fa);
+    public static final Color purple100 = new Color(0xffe3ddf2);
+    public static final Color purple200 = new Color(0xffc1b5e3);
+    public static final Color purple300 = new Color(0xff957fce);
+    public static final Color purple400 = new Color(0xff7356bf);
+    public static final Color purple500 = new Color(0xff574191);
+    public static final Color purple600 = new Color(0xff453473);
+    public static final Color purple700 = new Color(0xff2e224c);
+
+    // Yellow
+    public static final Color yellow50 = new Color(0xfffffaf0);
+    public static final Color yellow100 = new Color(0xfffff2d9);
+    public static final Color yellow200 = new Color(0xffffe3ac);
+    public static final Color yellow300 = new Color(0xffffcf70);
+    public static final Color yellow400 = new Color(0xffffc043);
+    public static final Color yellow500 = new Color(0xffbc8b2c);
+    public static final Color yellow600 = new Color(0xff997328);
+    public static final Color yellow700 = new Color(0xff664d1b);
+
+    // Brown
+    public static final Color brown50 = new Color(0xfff7f3f1);
+    public static final Color brown100 = new Color(0xffebe0db);
+    public static final Color brown200 = new Color(0xffd2bbb0);
+    public static final Color brown300 = new Color(0xffb18977);
+    public static final Color brown400 = new Color(0xff99644c);
+    public static final Color brown500 = new Color(0xff744c3a);
+    public static final Color brown600 = new Color(0xff5c3c2e);
+    public static final Color brown700 = new Color(0xff3d281e);
 
     // Gray
     public static final Color gray50 = new Color(0xfff6f6f6);
     public static final Color gray100 = new Color(0xffeeeeee);
     public static final Color gray200 = new Color(0xffe2e2e2);
+    public static final Color gray300 = new Color(0xffcbcbcb);
     public static final Color gray400 = new Color(0xffafafaf);
     public static final Color gray500 = new Color(0xff757575);
     public static final Color gray600 = new Color(0xff545454);
+    public static final Color gray700 = new Color(0xff333333);
+
+    // Secondary
+    public static final Color blue = blue400;
+    public static final Color red = red400;
+    public static final Color orange = orange400;
+    public static final Color yellow = yellow400;
+    public static final Color green = green400;
+    public static final Color purple = purple400;
+    public static final Color brown = brown400;
 
     // Primary Button
-    public static final Color buttonPrimaryFill = new Color(0xff000000);
-    public static final Color buttonPrimaryText = new Color(0xffffffff);
-    public static final Color buttonPrimaryHover = new Color(0xff333333);
-    public static final Color buttonPrimaryActive = new Color(0xff545454);
+    public static final Color buttonPrimaryFill = black;
+    public static final Color buttonPrimaryText = white;
+    public static final Color buttonPrimaryHover = gray700;
+    public static final Color buttonPrimaryActive = gray600;
 
     // Secondary Button
-    public static final Color buttonSecondaryFill = new Color(0xffeeeeee);
-    public static final Color buttonSecondaryText = new Color(0xff000000);
-    public static final Color buttonSecondaryHover = new Color(0xffe2e2e2);
-    public static final Color buttonSecondaryActive = new Color(0xffcbcbcb);
+    public static final Color buttonSecondaryFill = gray100;
+    public static final Color buttonSecondaryText = black;
+    public static final Color buttonSecondaryHover = gray200;
+    public static final Color buttonSecondaryActive = gray300;
 
     // Tertiary Button
-    public static final Color buttonTertiaryFill = new Color(0x00ffffff);
-    public static final Color buttonTertiaryText = new Color(0xff000000);
-    public static final Color buttonTertiaryHover = new Color(0xfff6f6f6);
-    public static final Color buttonTertiaryActive = new Color(0xffeeeeee);
-    public static final Color buttonTertiarySelectedFill = new Color(0xffeeeeee);
-    public static final Color buttonTertiarySelectedText = new Color(0xff000000);
+    public static final Color buttonTertiaryFill = white;
+    public static final Color buttonTertiaryText = black;
+    public static final Color buttonTertiaryHover = gray50;
+    public static final Color buttonTertiaryActive = gray100;
+    public static final Color buttonTertiarySelectedFill = gray100;
+    public static final Color buttonTertiarySelectedText = black;
 
     // Disabled Button
-    public static final Color buttonDisabledFill = new Color(0xfff6f6f6);
-    public static final Color buttonDisabledText = new Color(0xffafafaf);
+    public static final Color buttonDisabledFill = gray50;
+    public static final Color buttonDisabledText = gray400;
 
 }

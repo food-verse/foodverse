7e33992d546c1d4ebefd3d0afddc9754a662435a
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-11 20:10:02 +0300

    Add support for loading svg assets

--- src/main/java/com/foodverse/utility/system/AssetManager.java
+++ src/main/java/com/foodverse/utility/system/AssetManager.java
@@ -2,11 +2,14 @@ package com.foodverse.utility.system;
 
 import java.awt.Font;
 import java.awt.FontFormatException;
+import java.awt.Graphics2D;
 import java.awt.GraphicsEnvironment;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Optional;
 import java.util.logging.Level;
@@ -14,6 +17,9 @@ import java.util.logging.Logger;
 import java.util.stream.Stream;
 
 import javax.imageio.ImageIO;
+import com.kitfox.svg.SVGDiagram;
+import com.kitfox.svg.SVGException;
+import com.kitfox.svg.SVGUniverse;
 
 public final class AssetManager {
 
@@ -62,6 +68,35 @@ public final class AssetManager {
             logger.log(Level.INFO, "Could not load image from: {0}", source);
         }
         return Optional.ofNullable(scaledImage);
+    }
+
+    public static Optional<Image> getVector(File vectorFile) {
+        BufferedImage bufferedImage = null;
+        try {
+            // Create an SVGUniverse
+            SVGUniverse universe = new SVGUniverse();
+
+            // Load the SVG file into memory
+            URL address = vectorFile.toURI().toURL();
+            SVGDiagram diagram = universe.getDiagram(universe.loadSVG(address));
+
+            // Create a Graphics2D that the SVG should be rendered to
+            bufferedImage = new BufferedImage(
+                    Math.round(diagram.getWidth()),
+                    Math.round(diagram.getHeight()),
+                    BufferedImage.TYPE_INT_ARGB);
+            Graphics2D graphics = bufferedImage.createGraphics();
+            graphics.setRenderingHint(
+                    RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            graphics.setRenderingHint(
+                    RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+            // Render the SVG to the Graphics2D
+            diagram.render(graphics);
+        } catch (MalformedURLException | SVGException e) {
+            logger.log(Level.INFO, "Could not load image from: {0}", vectorFile.getAbsolutePath());
+        }
+        return Optional.ofNullable(bufferedImage);
     }
 
 }

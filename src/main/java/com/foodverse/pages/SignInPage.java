5335cdd32f02cab30249530fcbc3eefb991255f4
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-12 11:50:41 +0300

    Add Sign In to Home flow

--- src/main/java/com/foodverse/pages/SignInPage.java
+++ src/main/java/com/foodverse/pages/SignInPage.java
@@ -5,6 +5,7 @@ import javax.swing.JPanel;
 import com.foodverse.utility.navigation.Page;
 import com.foodverse.utility.navigation.Pages;
 import com.foodverse.utility.navigation.Router;
+import com.foodverse.utility.system.Database;
 import com.foodverse.utility.ui.Button.ButtonSize;
 import com.foodverse.utility.ui.Button.ButtonType;
 import com.foodverse.widgets.button.RectButton;
@@ -16,6 +17,10 @@ public final class SignInPage extends Page {
     private final Component component;
 
     public SignInPage() {
+
+        // Getting a reference to the database...
+        var db = Database.getInstance();
+
         // Heading
         var panel = new JPanel();
         var text = new Heading("SignInPage", HeadingSize.L);
@@ -33,9 +38,19 @@ public final class SignInPage extends Page {
                 e -> {
                     Router.pushPage(Pages.OVERVIEW);
                 });
+        var signInButton = new RectButton(
+                "Sign In ->",
+                ButtonSize.S,
+                ButtonType.TERTIARY,
+                e -> {
+                    if (db.signIn("emilysmith123", "XyZ987!")) {
+                        Router.pushPage(Pages.HOME);
+                    }
+                });
         panel.add(text.getRef());
         panel.add(openOnboardingPage.getRef());
         panel.add(openOverviewPage.getRef());
+        panel.add(signInButton.getRef());
         panel.setOpaque(false);
         component = panel;
     }

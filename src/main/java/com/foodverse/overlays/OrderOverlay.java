800fcceab76b651b4093e71921c4cd191d36ea5f
Theognosia <127968532+Theognosia@users.noreply.github.com>
2023-04-19 00:27:59 +0300

    order changes

--- src/main/java/com/foodverse/overlays/OrderOverlay.java
+++ src/main/java/com/foodverse/overlays/OrderOverlay.java
@@ -1,33 +1,31 @@
 package com.foodverse.overlays;
 
 import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
-import javax.swing.BorderFactory;
 
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-import javax.swing.border.Border;
 import com.foodverse.utility.navigation.Overlay;
 import com.foodverse.utility.navigation.Router;
 import com.foodverse.utility.ui.Button.ButtonSize;
 import com.foodverse.utility.ui.Button.ButtonType;
-import com.foodverse.utility.ui.Colors;
+
 import com.foodverse.views.AddressView;
 import com.foodverse.views.AmountView;
 import com.foodverse.views.PaymentView;
+import com.foodverse.views.ProductView;
+import com.foodverse.views.TipsView;
 import com.foodverse.widgets.button.PillButton;
 import com.foodverse.widgets.button.RectButton;
 import com.foodverse.widgets.text.Heading;
 import com.foodverse.widgets.text.Heading.HeadingSize;
-import com.foodverse.widgets.text.Label;
-import com.foodverse.widgets.text.Label.LabelSize;
 
 public final class OrderOverlay extends Overlay {
 
         @Override
         public Component getRef() {
+
                 // Heading
                 var panel = new JPanel();
                 var orderLabel = new Heading("Order", HeadingSize.XL);
@@ -40,9 +38,6 @@ public final class OrderOverlay extends Overlay {
                                 });
                 panel.add(button.getRef());
                 panel.add(orderLabel.getRef());
-
-                // Border
-                Border boarder = BorderFactory.createLineBorder(Colors.black);
 
                 // Address
                 AddressView view = new AddressView();
@@ -58,46 +53,12 @@ public final class OrderOverlay extends Overlay {
                 panel.add(amount.getRef());
 
                 // Products
-                var products = new JPanel();
-                products.setBorder(boarder);
-                products.setPreferredSize(new Dimension(1000, 200));
-                products.setBackground(Colors.white);
-                panel.add(products);
+                ProductView products = new ProductView();
+                panel.add(products.getRef());
 
                 // Tips
-                var tips = new JPanel();
-                tips.setBorder(boarder);
-                tips.setPreferredSize(new Dimension(1200, 80));
-                tips.setBackground(Colors.white);
-                var tipLabel = new Label("Tips:", LabelSize.L);
-                tips.add(tipLabel.getRef());
-
-                var tip1 = new PillButton("0.5$",
-                                ButtonSize.XS,
-                                ButtonType.SECONDARY,
-                                e -> {
-                                });
-                var tip2 = new PillButton("1$",
-                                ButtonSize.XS,
-                                ButtonType.SECONDARY,
-                                e -> {
-                                });
-                var tip3 = new PillButton("2$",
-                                ButtonSize.XS,
-                                ButtonType.SECONDARY,
-                                e -> {
-                                });
-                var tip4 = new PillButton("5$",
-                                ButtonSize.XS,
-                                ButtonType.SECONDARY,
-                                e -> {
-                                });
-
-                tips.add(tip1.getRef());
-                tips.add(tip2.getRef());
-                tips.add(tip3.getRef());
-                tips.add(tip4.getRef());
-                panel.add(tips);
+                TipsView tips = new TipsView();
+                panel.add(tips.getRef());
 
                 var checkoutButton = new RectButton("Checkout ->",
                                 ButtonSize.S,

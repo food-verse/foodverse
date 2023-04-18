deb52eac788701d9a6c451cce430063480911207
Theognosia <127968532+Theognosia@users.noreply.github.com>
2023-04-19 00:45:55 +0300

    remove boarder

--- src/main/java/com/foodverse/views/AmountView.java
+++ src/main/java/com/foodverse/views/AmountView.java
@@ -6,7 +6,6 @@ import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import com.foodverse.utility.core.Widget;
 import com.foodverse.utility.ui.Colors;
-import javax.swing.border.Border;
 import com.foodverse.widgets.text.Label;
 import com.foodverse.widgets.text.Label.LabelSize;
 
@@ -19,12 +18,12 @@ public final class AmountView extends Widget {
         panel.setPreferredSize(new Dimension(1250, 100));
         panel.setBackground(Colors.white);
 
-        // Border
-        Border boarder = BorderFactory.createLineBorder(Colors.black);
+        // // Border
+        // Border boarder = BorderFactory.createLineBorder(Colors.black);
 
         var amount = new JPanel();
         amount = new JPanel();
-        amount.setBorder(boarder);
+        // amount.setBorder(boarder);
         amount.setPreferredSize(new Dimension(600, 100));
         var amountLabel = new Label("Total:", LabelSize.L);
         amount.add(amountLabel.getRef());

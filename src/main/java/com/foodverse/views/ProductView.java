b8b366360275e7b4128cefbbc587adf288df3d40
Theognosia <127968532+Theognosia@users.noreply.github.com>
2023-04-19 00:46:26 +0300

    remove boarder

--- src/main/java/com/foodverse/views/ProductView.java
+++ src/main/java/com/foodverse/views/ProductView.java
@@ -6,7 +6,6 @@ import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import com.foodverse.utility.core.Widget;
 import com.foodverse.utility.ui.Colors;
-import javax.swing.border.Border;
 
 public final class ProductView extends Widget {
 
@@ -17,11 +16,8 @@ public final class ProductView extends Widget {
         panel.setPreferredSize(new Dimension(1250, 100));
         panel.setBackground(Colors.white);
 
-        // Border
-        Border boarder = BorderFactory.createLineBorder(Colors.black);
-
         var products = new JPanel();
-        products.setBorder(boarder);
+
         products.setPreferredSize(new Dimension(1000, 200));
         products.setBackground(Colors.white);
         panel.add(products);

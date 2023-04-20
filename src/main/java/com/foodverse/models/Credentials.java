b062934a00178be7f704c8948f3a8c2466bb43d9
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-20 13:57:02 +0300

    Refactor model classes into records

--- src/main/java/com/foodverse/models/Credentials.java
+++ src/main/java/com/foodverse/models/Credentials.java
@@ -4,37 +4,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
 import com.google.gson.annotations.SerializedName;
 
-public final class Credentials {
-
-    @SerializedName("password")
-    private String password;
-
-    @SerializedName("recovery_answers")
-    private Map<Integer, String> recoveryAnswers;
+public record Credentials(
+    @SerializedName("password") String password,
+    @SerializedName("recovery_answers") Map<Integer, String> recoveryAnswers) {
 
     public Credentials(String password, List<String> recoveryAnswers) {
-        this.password = password;
-        this.recoveryAnswers = IntStream.range(0, recoveryAnswers.size())
-                .boxed()
-                .collect(Collectors.toMap(i -> i, recoveryAnswers::get));
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Map<Integer, String> getRecoveryAnswers() {
-        return recoveryAnswers;
-    }
-
-    public void setRecoveryAnswers(Map<Integer, String> recoveryAnswers) {
-        this.recoveryAnswers = recoveryAnswers;
+        this(password, IntStream.range(0, recoveryAnswers.size())
+            .boxed()
+            .collect(Collectors.toMap(i -> i, recoveryAnswers::get)));
     }
 
 }
--- src/main/java/com/foodverse/models/Order.java
+++ src/main/java/com/foodverse/models/Order.java
@@ -2,96 +2,15 @@ package com.foodverse.models;
 
 import java.util.Date;
 import java.util.Map;
+
 import com.google.gson.annotations.SerializedName;
 
-public final class Order {
-
-    @SerializedName("merchant")
-    private String merchant;
-
-    @SerializedName("date")
-    private Date date;
-
-    @SerializedName("items")
-    private Map<String, Integer> items;
-
-    @SerializedName("delivery_tip")
-    private float deliveryTip;
-
-    @SerializedName("total")
-    private float total;
-
-    @SerializedName("payment")
-    private PaymentMethod method;
-
-    @SerializedName("type")
-    private OrderType type;
-
-    public Order(String merchant, Date date, Map<String, Integer> items, float deliveryTip,
-            float total, PaymentMethod method, OrderType type) {
-        this.merchant = merchant;
-        this.date = date;
-        this.items = items;
-        this.deliveryTip = deliveryTip;
-        this.total = total;
-        this.method = method;
-        this.type = type;
-    }
-
-    public String getMerchant() {
-        return merchant;
-    }
-
-    public void setMerchant(String merchant) {
-        this.merchant = merchant;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public Map<String, Integer> getItems() {
-        return items;
-    }
-
-    public void setItems(Map<String, Integer> items) {
-        this.items = items;
-    }
-
-    public float getDeliveryTip() {
-        return deliveryTip;
-    }
-
-    public void setDeliveryTip(float deliveryTip) {
-        this.deliveryTip = deliveryTip;
-    }
-
-    public float getTotal() {
-        return total;
-    }
-
-    public void setTotal(float total) {
-        this.total = total;
-    }
-
-    public PaymentMethod getMethod() {
-        return method;
-    }
-
-    public void setMethod(PaymentMethod method) {
-        this.method = method;
-    }
-
-    public OrderType getType() {
-        return type;
-    }
-
-    public void setType(OrderType type) {
-        this.type = type;
-    }
-
+public record Order(
+    @SerializedName("merchant") String merchant,
+    @SerializedName("date") Date date,
+    @SerializedName("items") Map<String, Integer> items,
+    @SerializedName("delivery_tip") float deliveryTip,
+    @SerializedName("total") float total,
+    @SerializedName("payment") PaymentMethod method,
+    @SerializedName("type") OrderType type) {
 }

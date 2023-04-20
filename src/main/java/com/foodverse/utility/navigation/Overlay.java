85e1016ec47774dc20ba4241801a5d5523e98ea9
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-20 12:32:11 +0300

    Update overlay to handle its own close operation

--- src/main/java/com/foodverse/utility/navigation/Overlay.java
+++ src/main/java/com/foodverse/utility/navigation/Overlay.java
@@ -1,7 +1,11 @@
 package com.foodverse.utility.navigation;
 
 import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.function.Consumer;
 import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 import com.foodverse.utility.core.Widget;
 
 /**
@@ -16,26 +20,35 @@ public abstract class Overlay extends Widget implements Identifiable {
     // The preferred size for the page
     private final Dimension dimension;
 
+    // A callback to be executed when the window is closed
+    private final Consumer<WindowEvent> onClose;
+
     /**
      * Creates a new {@link Overlay} with the specified width and height.
      *
      * @param width The preferred width for the page
      * @param height The preferred height for the page
+     * @param onClose A callback to be executed when the window is closed
      */
-    protected Overlay(int width, int height) {
-        inflate(this);
+    protected Overlay(int width, int height, Consumer<WindowEvent> onClose) {
+        this.onClose = onClose;
         dimension = new Dimension(width, height);
         frame.getContentPane().setBackground(Shell.getOptions().getBackgroundColor());
         frame.setTitle(getId());
+        frame.addWindowListener(new OverlayListener());
+        frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
     }
 
     /**
      * Creates a new {@link Overlay} with no specified dimensions.
      */
     protected Overlay() {
-        this(0, 0);
+        this(0, 0, null);
     }
 
+    /**
+     * Returns the underlying JFrame of the overlay.
+     */
     protected JFrame getFrame() {
         return frame;
     }
@@ -75,6 +88,21 @@ public abstract class Overlay extends Widget implements Identifiable {
      */
     public void close() {
         frame.setVisible(false);
+    }
+
+    /**
+     * A listener to handle the window closing event and execute the onClose callback.
+     */
+    private class OverlayListener extends WindowAdapter {
+
+        @Override
+        public void windowClosing(WindowEvent e) {
+            if (onClose != null) {
+                onClose.accept(e);
+            }
+            Router.closeOverlay();
+        }
+
     }
 
 }

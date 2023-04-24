7f72ad5118f96dcef46e5c67e83d1e4f8366c194
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-24 21:06:38 +0300

    Cleanup

--- src/main/java/com/foodverse/overlays/ProfileInfoOverlay.java
+++ src/main/java/com/foodverse/overlays/ProfileInfoOverlay.java
@@ -5,7 +5,6 @@ import javax.swing.JPanel;
 import com.foodverse.utility.navigation.Overlay;
 import com.foodverse.utility.navigation.Router;
 import com.foodverse.widgets.button.RectButton;
-import com.foodverse.widgets.layout.SizedBox;
 import com.foodverse.widgets.text.Heading;
 import com.foodverse.widgets.text.Heading.HeadingSize;
 import com.foodverse.utility.ui.TextField;

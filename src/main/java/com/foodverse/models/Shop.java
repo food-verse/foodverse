947043cf98c3f2e6710405c186ef62de28da72f3
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-27 13:18:25 +0300

    Add phone field and withRating to Shop class

--- src/main/java/com/foodverse/models/Shop.java
+++ src/main/java/com/foodverse/models/Shop.java
@@ -2,6 +2,7 @@ package com.foodverse.models;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import com.foodverse.widgets.media.AssetSize;
 import com.google.gson.annotations.SerializedName;
@@ -9,6 +10,7 @@ import com.google.gson.annotations.SerializedName;
 public record Shop(
     @SerializedName("name") String name,
     @SerializedName("address") String address,
+    @SerializedName("phone") String phone,
     @SerializedName("rating") float rating,
     @SerializedName("reviews") int reviews,
     @SerializedName("type") ShopType type,
@@ -19,8 +21,28 @@ public record Shop(
     @SerializedName("menu") List<Item> menu) {
 
     public Shop withRating(float rating) {
-        return new Shop(name, address, rating, reviews, type, thumbnails, prepTime, minOrder,
+        return new Shop(name, address, phone, rating, reviews, type, thumbnails, prepTime, minOrder,
             offers, menu);
+    }
+
+    public Shop withReviews(int reviews) {
+        return new Shop(name, address, phone, rating, reviews, type, thumbnails, prepTime, minOrder,
+            offers, menu);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Shop shop = (Shop) o;
+        return name.equals(shop.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 
 }

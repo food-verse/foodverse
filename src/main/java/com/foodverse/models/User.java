66fd76c8d6309f8dda7a43c3eb60c287641e03d8
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-04-27 12:24:31 +0300

    Add favorites to user and override equals

--- src/main/java/com/foodverse/models/User.java
+++ src/main/java/com/foodverse/models/User.java
@@ -1,6 +1,7 @@
 package com.foodverse.models;
 
 import java.util.List;
+import java.util.Objects;
 
 import com.google.gson.annotations.SerializedName;
 
@@ -11,10 +12,26 @@ public record User(
     @SerializedName("phone") String phone,
     @SerializedName("email") String email,
     @SerializedName("credentials") Credentials credentials,
+    @SerializedName("favorites") List<String> favorites,
     @SerializedName("orders") List<Order> orders) {
 
     public User withCredentials(Credentials credentials) {
-        return new User(id, name, addresses, phone, email, credentials, orders);
+        return new User(id, name, addresses, phone, email, credentials, favorites, orders);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
 }

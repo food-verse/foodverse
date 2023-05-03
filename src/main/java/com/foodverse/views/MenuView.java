d19ce5320686ea0ced8801bb729148a39fe936bb
Theognosia <127968532+Theognosia@users.noreply.github.com>
2023-05-03 18:57:53 +0300

    MenuView

--- src/main/java/com/foodverse/views/MenuView.java
+++ src/main/java/com/foodverse/views/MenuView.java
@@ -9,28 +9,24 @@ import javax.swing.JScrollPane;
 import com.foodverse.utility.core.Widget;
 import java.awt.Font;
 
+public final class MenuView extends Widget {
 
-public class MenuView {
+    @Override
+    public Component getRef() {
+        var panel = new JPanel();
 
-    public final class AmountView extends Widget {
+        var menu = new JLabel("Menu  ");
+        menu.setFont(new Font("Courier New", Font.BOLD, 25));
+        menu.setForeground(Color.white);
 
-        @Override
-        public Component getRef() {
-            var panel = new JPanel();
+        panel.setPreferredSize(new Dimension(1300, 300));
+        panel.setBackground(Color.lightGray);
+        panel.add(menu);
 
-            var menu = new JLabel("Menu  ");
-            menu.setFont(new Font("Courier New", Font.BOLD, 25));
-            menu.setForeground(Color.white);
+        JScrollPane scroll2 = new JScrollPane(panel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        panel.add(scroll2);
 
-            panel.setPreferredSize(new Dimension(1300, 300));
-            panel.setBackground(Color.lightGray);
-            panel.add(menu);
-
-            JScrollPane scroll2 = new JScrollPane(panel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-                    JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-            panel.add(scroll2);
-
-            return panel;
-        }
+        return panel;
     }
 }

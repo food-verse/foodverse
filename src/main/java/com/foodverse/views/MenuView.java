0a70252b04ba715e31bfeda7b56160a4dcd03780
SofiaKal <128231824+SofiaKal@users.noreply.github.com>
2023-05-09 12:59:38 +0300

    Menu for each shop

--- src/main/java/com/foodverse/views/MenuView.java
+++ src/main/java/com/foodverse/views/MenuView.java
@@ -7,42 +7,36 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import com.foodverse.models.Shop;
 import com.foodverse.utility.core.Widget;
-import com.foodverse.utility.system.Database;
+// import com.foodverse.utility.system.Database;
 import com.foodverse.utility.ui.Button.ButtonSize;
 import com.foodverse.utility.ui.Button.ButtonType;
 import com.foodverse.widgets.button.PillButton;
 import java.awt.Font;
-import java.util.List;
+import java.util.Optional;
 
 public final class MenuView extends Widget {
+    private final Component component;
 
-    private final Database db = Database.getInstance();
+    // private final Database db = Database.getInstance();
 
-    @Override
-    public Component getRef() {
+    public MenuView(Optional<Shop> shop) {
 
         var panel = new JPanel();
-
-        List<Shop> shops = db.getShops();
-
 
         var menu = new JLabel("Menu  ");
         menu.setFont(new Font("Courier New", Font.BOLD, 25));
         menu.setForeground(Color.black);
 
-
         panel.add(menu);
 
-
-        for (int i = 0; i < shops.get(0).menu().size(); i++) {
+        for (int i = 0; i < shop.get().menu().size(); i++) {
 
             var itemPanel = new JPanel();
 
             itemPanel.setPreferredSize(new Dimension(1500, 60));
 
-
-            var x = shops.get(0).menu().get(i).name();
-            var price = shops.get(0).menu().get(i).price();
+            var x = shop.get().menu().get(i).name();
+            var price = shop.get().menu().get(i).price();
             var priceAsAString = Float.toString(price);
             var menuName = new JLabel(x);
             var menuPrice = new JLabel(priceAsAString);
@@ -59,16 +53,19 @@ public final class MenuView extends Widget {
 
         }
 
-
-        // JScrollPane scroll2 = new JScrollPane(panel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+        // JScrollPane scroll2 = new JScrollPane(panel,
+        // JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
         // JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         // panel.add(scroll2);
-
 
         panel.setPreferredSize(new Dimension(1400, 300));
         panel.setBackground(Color.white);
 
+        component = panel;
+    }
 
-        return panel;
+    @Override
+    public Component getRef() {
+        return component;
     }
 }

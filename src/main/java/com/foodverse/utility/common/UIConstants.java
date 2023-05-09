8c7fb438732fb639701aa2c326bf0431e5e04625
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-05-09 16:30:56 +0300

    Add new class for saving the strings used in widgets

---
+++ src/main/java/com/foodverse/utility/common/UIConstants.java
@@ -0,0 +1,9 @@
+package com.foodverse.utility.common;
+
+public final class UIConstants {
+
+    private UIConstants() {}
+
+    public static final String EMPTY_STATE_MESSAGE = "Unfortunately, there is nothing to display.";
+
+}

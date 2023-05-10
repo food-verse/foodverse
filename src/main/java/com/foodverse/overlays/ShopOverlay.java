8aab63881a35d60d9a67d950d34a8d7d9e5b9f97
SofiaKal <128231824+SofiaKal@users.noreply.github.com>
2023-05-10 19:59:10 +0300

    shop

--- src/main/java/com/foodverse/overlays/ShopOverlay.java
+++ src/main/java/com/foodverse/overlays/ShopOverlay.java
@@ -1,18 +1,33 @@
 package com.foodverse.overlays;
 
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.Font;
 import com.foodverse.models.Shop;
 import com.foodverse.utility.layout.Align;
 import com.foodverse.utility.layout.EdgeInsets;
 import com.foodverse.utility.navigation.Overlay;
+import com.foodverse.utility.navigation.Router;
 import com.foodverse.utility.system.Database;
 import com.foodverse.utility.ui.ImageStyle;
+import com.foodverse.utility.ui.Button.ButtonSize;
+import com.foodverse.utility.ui.Button.ButtonType;
 import com.foodverse.views.EmptyView;
+import com.foodverse.views.Offers;
+import com.foodverse.views.RateView;
+import com.foodverse.widgets.button.PillButton;
+import com.foodverse.widgets.button.RectButton;
 import com.foodverse.widgets.layout.Column;
+import com.foodverse.widgets.layout.ListTile;
+import com.foodverse.widgets.layout.ScrollView;
 import com.foodverse.widgets.media.IconAsset;
 import com.foodverse.widgets.media.Image;
 import com.foodverse.widgets.media.VectorImage;
@@ -23,7 +38,7 @@ public final class ShopOverlay extends Overlay {
 
     // Getting a reference to the database...
     private final Database db = Database.getInstance();
-
+    // private final Component component;
     private final String merchant;
 
     public ShopOverlay(String merchant) {
@@ -41,8 +56,11 @@ public final class ShopOverlay extends Overlay {
         }
 
         // pass the name of the shop in a variable
-        var name = " ";
-        name = shop.get().name();
+        // var name = " ";
+        var name = shop.get().name();
+
+        // props.name();
+
 
         var column = new Column();
 
@@ -67,6 +85,9 @@ public final class ShopOverlay extends Overlay {
                 new ImageStyle.Builder().width(800).height(200).build());
         var heartImage = new VectorImage(IconAsset.HEART);
         var heartFillImage = new VectorImage(IconAsset.HEART_FILL);
+
+        // Create a map for the choosen items
+        Map<String, Integer> addproducts = new HashMap<>();
 
         // Add padding to heart
         var paddedImage = new Column();
@@ -97,12 +118,76 @@ public final class ShopOverlay extends Overlay {
 
         panel.add(heartImage.getRef());
 
+        // offers
+        // var offers = new JLabel("Offers");
+        var offersTile = new ListTile("Offers");
+
+        panel.add(offersTile.getRef());
+        // offersTile.setFont(new Font("Courier New", Font.BOLD, 25));
+        // offersTile.setForeground(Color.black);
+
+        if (shop.get().offers().isEmpty()) {
+            panel.add(new EmptyView().getRef());
+        } else {
+            panel.add(new Offers(shop).getRef());
+        }
+
+        // props.offers();
+
+        // Menu
+        var menu = new JLabel("Menu  ");
+        menu.setFont(new Font("Courier New", Font.BOLD, 25));
+        menu.setForeground(Color.black);
+
+        panel.add(menu);
+
+        for (int i = 0; i < shop.get().menu().size(); i++) {
+
+            var itemPanel = new JPanel();
+
+            itemPanel.setPreferredSize(new Dimension(1500, 60));
+
+            var x = shop.get().menu().get(i).name();
+            var price = shop.get().menu().get(i).price();
+            var priceAsAString = Float.toString(price);
+            var menuName = new JLabel(x);
+            var menuPrice = new JLabel(priceAsAString);
+
+            var productAddButton = new PillButton("Add", ButtonSize.XS, ButtonType.SECONDARY, e -> {
+                addproducts.put(x, 1);
+            });
+
+            itemPanel.add(menuName);
+            itemPanel.add(menuPrice);
+            itemPanel.add(productAddButton.getRef());
+
+            panel.add(itemPanel);
+
+        }
+
+        panel.setPreferredSize(new Dimension(1400, 300));
+        panel.setBackground(Color.white);
+
+        // Rate
+        RateView rate = new RateView(shop);
+        panel.add(rate.getRef());
+
+        // RectButton "Cart" to appear the OrderOverlay
+        var OrderButton = new RectButton("Cart", ButtonSize.S, ButtonType.PRIMARY, e -> {
+            Router.closeOverlay();
+            Router.openOverlay(new OrderOverlay(merchant, addproducts));
+
+        });
+
+        panel.add(OrderButton.getRef());
+
+        // component = new ScrollView(panel).getRef();
+
         column.addWidget(thumbnailImage, new EdgeInsets.Builder().all(0).build(),
                 Align.FIRST_LINE_START);
         column.addComponent(panel, new EdgeInsets.Builder().all(0).build(), Align.LAST_LINE_START);
         return column.getRef();
+        // return new ScrollView(panel).getRef();
     }
-
-
 
 }

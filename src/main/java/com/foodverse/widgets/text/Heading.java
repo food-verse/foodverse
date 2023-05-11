de32a8cfc58d91a7c6d3a8bb61f3c07d6456905e
georgiadisxc <128224291+georgiadisxc@users.noreply.github.com>
2023-05-11 19:31:31 +0300

    Cleanup text styles and use the new TextStyle and FontStyle classes

--- src/main/java/com/foodverse/widgets/text/Heading.java
+++ src/main/java/com/foodverse/widgets/text/Heading.java
@@ -3,46 +3,33 @@ package com.foodverse.widgets.text;
 import java.awt.Color;
 
 import com.foodverse.utility.navigation.Shell;
-import com.foodverse.utility.ui.Colors;
+import com.foodverse.utility.ui.FontStyle;
+import com.foodverse.utility.ui.FontStyle.FontWeight;
 import com.foodverse.utility.ui.Text;
 import com.foodverse.utility.ui.TextStyle;
-import com.foodverse.utility.ui.TextStyle.FontWeight;
 
 public final class Heading extends Text {
 
     public Heading(String data, HeadingSize fontSize) {
-        this(data, fontSize, false, Colors.black);
+        this(data, fontSize, new TextStyle.Builder().build());
     }
 
     public Heading(String data, HeadingSize fontSize, Color color) {
-        this(data, fontSize, false, color);
+        this(data, fontSize, new TextStyle.Builder().color(color).build());
     }
 
-    public Heading(String data, HeadingSize fontSize, boolean isMono) {
-        super(data, getTextStyle(fontSize, isMono, Colors.black));
+    public Heading(String data, HeadingSize fontSize, TextStyle textStyle) {
+        super(data, getFontStyle(fontSize, textStyle.isMono()), textStyle);
     }
 
-    public Heading(String data, HeadingSize fontSize, boolean isMono, Color color) {
-        super(data, getTextStyle(fontSize, isMono, color));
-    }
-
-    public static TextStyle getTextStyle(HeadingSize fontSize, boolean isMono, Color color) {
-        return new TextStyle.Builder()
+    public static FontStyle getFontStyle(HeadingSize fontSize, boolean isMono) {
+        return new FontStyle.Builder()
             .family(isMono
                 ? Shell.getOptions().getMonospacedFont()
                 : Shell.getOptions().getDefaultFont())
             .weight(FontWeight.MEDIUM)
             .size(fontSize.getSize())
-            .color(color)
             .build();
-    }
-
-    public static TextStyle getTextStyle(HeadingSize fontSize, boolean isMono) {
-        return getTextStyle(fontSize, isMono, Colors.black);
-    }
-
-    public static TextStyle getTextStyle(HeadingSize fontSize) {
-        return getTextStyle(fontSize, false, Colors.black);
     }
 
     public enum HeadingSize {

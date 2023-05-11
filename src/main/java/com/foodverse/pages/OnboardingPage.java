fb35f497285e0c2d4093a51e89162c2bfe78a14e
georgiadisxc <128224291+georgiadisxc@users.noreply.github.com>
2023-05-11 23:34:16 +0300

    Onboarding page early prototype

--- src/main/java/com/foodverse/pages/OnboardingPage.java
+++ src/main/java/com/foodverse/pages/OnboardingPage.java
@@ -1,63 +1,171 @@
 package com.foodverse.pages;
 
 import java.awt.Component;
+import java.util.List;
+import java.util.Random;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
 import com.foodverse.overlays.InfoOverlay;
 import com.foodverse.overlays.SignInOverlay;
 import com.foodverse.overlays.SignUpOverlay;
+import com.foodverse.utility.common.UIConstants;
 import com.foodverse.utility.common.URLHandler;
+import com.foodverse.utility.layout.Align;
+import com.foodverse.utility.layout.EdgeInsets;
 import com.foodverse.utility.navigation.Page;
 import com.foodverse.utility.navigation.Router;
+import com.foodverse.utility.ui.Colors;
+import com.foodverse.utility.ui.ImageStyle;
 import com.foodverse.utility.ui.Button.ButtonSize;
 import com.foodverse.utility.ui.Button.ButtonType;
 import com.foodverse.widgets.button.PillButton;
+import com.foodverse.widgets.layout.Column;
+import com.foodverse.widgets.layout.Row;
+import com.foodverse.widgets.layout.ScrollView;
 import com.foodverse.widgets.media.IconAsset;
+import com.foodverse.widgets.media.Image;
 import com.foodverse.widgets.media.VectorImage;
+import com.foodverse.widgets.text.Display;
+import com.foodverse.widgets.text.Heading;
+import com.foodverse.widgets.text.Display.DisplaySize;
+import com.foodverse.widgets.text.Heading.HeadingSize;
 
 public final class OnboardingPage extends Page {
 
-    private final Component component;
+    private final JPanel panel;
+
+    private static Random random = new Random();
 
     public OnboardingPage() {
 
-        //
-        var panel = new JPanel();
-        panel.setOpaque(false);
+        List<String> onboardingImages = List.of(
+                "corn_meat_quesadillas_sauce_on_board.jpg",
+                "mexican_fajita_nachos_sombrero_pan.jpg",
+                "tasty_fajita_with_veggies_beef.jpg");
+
+        // Creating text widgets...
+        var promoTitleText = new Display(UIConstants.ONBOARDING_PROMO_TITLE, DisplaySize.XS);
+        var promoMessageText = new Heading(
+                UIConstants.ONBOARDING_PROMO_MESSAGE,
+                HeadingSize.S,
+                Colors.gray600);
+        var promoNoteText = new Heading(
+                UIConstants.ONBOARDING_PROMO_NOTE,
+                HeadingSize.XS,
+                Colors.gray600);
 
         // Creating image widgets...
         var repositoryUrl = "https://github.com/food-verse/foodverse";
         var brandImage = new VectorImage(IconAsset.BRAND, e -> {
             URLHandler.open(repositoryUrl);
         }, repositoryUrl);
+        var hushFaceImage = new Image("hush-emoji.png", new ImageStyle.Builder()
+                .width(32)
+                .height(32)
+                .build());
+        var onboardingImage = new Image(
+                onboardingImages.get(random.nextInt(onboardingImages.size())),
+                new ImageStyle.Builder()
+                        .width(600)
+                        .height(828)
+                        .build());
 
         // Creating button widgets...
         var infoButton = new PillButton(
-            "How it works",
-            ButtonSize.M,
-            ButtonType.TERTIARY,
-            e -> Router.openOverlay(new InfoOverlay()));
+                "How it works",
+                ButtonSize.M,
+                ButtonType.TERTIARY,
+                e -> Router.openOverlay(new InfoOverlay()));
         var signInButton = new PillButton(
-            "Sign In",
-            ButtonSize.M,
-            ButtonType.SECONDARY,
-            e -> Router.openOverlay(new SignInOverlay()));
+                "Sign In",
+                ButtonSize.M,
+                ButtonType.SECONDARY,
+                e -> Router.openOverlay(new SignInOverlay()));
         var signUpButton = new PillButton(
-            "Sign Up",
-            ButtonSize.M,
-            ButtonType.PRIMARY,
-            e -> Router.openOverlay(new SignUpOverlay()));
+                "Sign Up",
+                ButtonSize.M,
+                ButtonType.PRIMARY,
+                e -> Router.openOverlay(new SignUpOverlay()));
 
-        panel.add(brandImage.getRef());
-        panel.add(infoButton.getRef());
-        panel.add(signInButton.getRef());
-        panel.add(signUpButton.getRef());
-        component = panel;
+        // Creating main panel...
+        panel = new JPanel();
+        panel.setOpaque(false);
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        // Row with the brand's logo and the action buttons
+        // Creating parent panel for the images...
+        var headingRow = new JPanel();
+        headingRow.setOpaque(false);
+        headingRow.setLayout(new BoxLayout(headingRow, BoxLayout.X_AXIS));
+
+        var buttonRow = new Row();
+        buttonRow.addWidget(infoButton, new EdgeInsets.Builder()
+                .left(600)
+                .right(16)
+                .build(),
+                Align.LAST_LINE_END);
+        buttonRow.addWidget(signInButton, new EdgeInsets.Builder()
+                .right(16)
+                .build(),
+                Align.LAST_LINE_END);
+        buttonRow.addWidget(signUpButton, Align.LAST_LINE_END);
+
+        // Add images to the parent panel
+        headingRow.add(brandImage.getRef());
+        headingRow.add(Box.createHorizontalGlue());
+        headingRow.add(buttonRow.getRef());
+
+        // Add the padded heading row to the main panel
+        var paddedHeading = new Row();
+        paddedHeading.addComponent(headingRow, new EdgeInsets.Builder()
+                .symmetric(40, 48)
+                .bottom(36)
+                .build(),
+                Align.CENTER);
+        panel.add(paddedHeading.getRef());
+
+        var tempCol = new Column();
+        tempCol.addWidget(promoTitleText, new EdgeInsets.Builder()
+                .bottom(16)
+                .build(),
+                Align.FIRST_LINE_START);
+        tempCol.addWidget(promoMessageText, Align.LAST_LINE_END);
+
+        var tempRow2 = new Row();
+        tempRow2.addWidget(promoNoteText, Align.FIRST_LINE_START);
+        tempRow2.addWidget(hushFaceImage, Align.FIRST_LINE_END);
+
+        var tempCol2 = new Column();
+        tempCol2.addWidget(tempCol, new EdgeInsets.Builder()
+                .top(164)
+                .bottom(380)
+                .build(),
+                Align.FIRST_LINE_START);
+        tempCol2.addWidget(tempRow2, new EdgeInsets.Builder()
+                .build(),
+                Align.FIRST_LINE_START);
+
+        var tempRow = new Row();
+        tempRow.addWidget(tempCol2, Align.CENTER);
+        tempRow.addWidget(onboardingImage, Align.CENTER);
+
+        var paddedContent = new Column();
+        paddedContent.addWidget(tempRow, new EdgeInsets.Builder()
+                .all(48)
+                .top(0)
+                .build(),
+                Align.CENTER);
+
+        panel.add(tempRow.getRef());
+
     }
 
     @Override
     public Component getRef() {
-        return component;
+        // Wrap the main panel in a scroll view
+        return new ScrollView(panel).getRef();
     }
 
 }

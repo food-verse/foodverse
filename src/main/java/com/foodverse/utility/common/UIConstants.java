2e3483e2bb2f169f44ba0b835e97b5b18a29e24b
georgiadisxc <128224291+georgiadisxc@users.noreply.github.com>
2023-05-11 23:33:56 +0300

    Add ui text for the onboarding page

--- src/main/java/com/foodverse/utility/common/UIConstants.java
+++ src/main/java/com/foodverse/utility/common/UIConstants.java
@@ -4,6 +4,17 @@ public final class UIConstants {
 
     private UIConstants() {}
 
+    // Onboarding page
+    public static final String ONBOARDING_PROMO_TITLE =
+        "<html>Don't feel like cooking?<br>"
+            + "No problem*</html>";
+    public static final String ONBOARDING_PROMO_MESSAGE =
+        "<html>Browse through our vast selection of<br>"
+            + "restaurants and pay the way you<br>"
+            + "want.</html>";
+    public static final String ONBOARDING_PROMO_NOTE = "*We neither, but don’t tell anyone";
+
+    // Invalid credentials alert
     public static final String INVALID_CREDENTIALS_TITLE = "Invalid Credentials";
     public static final String INVALID_CREDENTIALS_DESCRIPTION =
         "<html>Sorry, we were unable to verify your login<br>"
@@ -12,6 +23,8 @@ public final class UIConstants {
             + "correctly. If you are still having trouble, you<br>"
             + "may reset your password or contact our<br>"
             + "support team for further assistance.</html>";
+
+    // Sign out alert
     public static final String SIGN_OUT_CONFIRM_MESSAGE = "Are you sure you want to sign out?";
     public static final String EMPTY_STATE_MESSAGE = "Unfortunately, there is nothing to display.";
 

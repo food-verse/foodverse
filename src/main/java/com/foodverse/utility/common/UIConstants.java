a305aa9bc2d00d3439614514ff12106227fb9818
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-05-11 22:21:37 +0300

    Show invalid credentials alert in case of unsuccessful login

--- src/main/java/com/foodverse/utility/common/UIConstants.java
+++ src/main/java/com/foodverse/utility/common/UIConstants.java
@@ -2,8 +2,17 @@ package com.foodverse.utility.common;
 
 public final class UIConstants {
 
-    private UIConstants() {}
+    private UIConstants() {
+    }
 
+    public static final String INVALID_CREDENTIALS_TITLE = "Invalid Credentials";
+    public static final String INVALID_CREDENTIALS_DESCRIPTION =
+        "<html>Sorry, we were unable to verify your login<br>"
+            + "credentials. Please ensure that you have<br>"
+            + "entered your email address and password<br>"
+            + "correctly. If you are still having trouble, you<br>"
+            + "may reset your password or contact our<br>"
+            + "support team for further assistance.</html>";
     public static final String SIGN_OUT_CONFIRM_MESSAGE = "Are you sure you want to sign out?";
     public static final String EMPTY_STATE_MESSAGE = "Unfortunately, there is nothing to display.";
 

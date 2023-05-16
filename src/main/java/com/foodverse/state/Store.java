2e2fa8b7500d47f893382a86afab3f3bb737cc45
georgiadisxc <128224291+georgiadisxc@users.noreply.github.com>
2023-05-16 23:21:39 +0300

    Add new class for storing global variables

---
+++ src/main/java/com/foodverse/state/Store.java
@@ -0,0 +1,12 @@
+package com.foodverse.state;
+
+import com.foodverse.models.PaymentMethod;
+import com.foodverse.utility.core.State;
+
+public final class Store {
+
+    private Store() {}
+
+    public static final State<PaymentMethod> orderPaymentMethod = new State<>(PaymentMethod.CARD);
+
+}

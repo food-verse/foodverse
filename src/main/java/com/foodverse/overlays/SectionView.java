3c8a6dbc9c0952ad1287d745aa2e52a1128b612b
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-05-17 22:49:52 +0300

    Create SectionView widget

---
+++ src/main/java/com/foodverse/overlays/SectionView.java
@@ -0,0 +1,44 @@
+package com.foodverse.overlays;
+
+import java.awt.Component;
+
+import com.foodverse.utility.common.UIConstants;
+import com.foodverse.utility.core.Widget;
+import com.foodverse.utility.layout.Align;
+import com.foodverse.utility.layout.EdgeInsets;
+import com.foodverse.utility.ui.Colors;
+import com.foodverse.widgets.layout.Column;
+import com.foodverse.widgets.text.Heading;
+import com.foodverse.widgets.text.Heading.HeadingSize;
+import com.foodverse.widgets.text.Paragraph;
+import com.foodverse.widgets.text.Paragraph.ParagraphSize;
+
+public final class SectionView extends Widget {
+
+    private final Component component;
+
+    public SectionView(String title, String description) {
+
+        // Creating text widgets...
+        var titleText = new Heading(UIConstants.INFO_STEP_3_TITLE, HeadingSize.S);
+        var descriptionText = new Paragraph(UIConstants.INFO_STEP_3_DESCRIPTION,
+            ParagraphSize.M,
+            Colors.gray600);
+
+        // Creating the column of the section...
+        var column = new Column();
+        column.addWidget(titleText, new EdgeInsets.Builder()
+                .bottom(16)
+                .build(),
+            Align.FIRST_LINE_START);
+        column.addWidget(descriptionText, Align.FIRST_LINE_START);
+
+        component = column.getRef();
+    }
+
+    @Override
+    public Component getRef() {
+        return component;
+    }
+
+}

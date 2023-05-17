b727dfeb43f37432685104f89752b1ebe02613fc
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-05-17 22:50:13 +0300

    Implement the InfoOverlay and update UIConstants

--- src/main/java/com/foodverse/utility/common/UIConstants.java
+++ src/main/java/com/foodverse/utility/common/UIConstants.java
@@ -14,6 +14,26 @@ public final class UIConstants {
             + "want.</html>";
     public static final String ONBOARDING_PROMO_NOTE = "*We neither, but don’t tell anyone";
 
+    // Info overlay
+    public static final String INFO_TITLE =
+        "<html>Order delivery online in 3<br>" +
+            "simple steps</html>";
+    public static final String INFO_STEP_1_TITLE = "Step 1: Enter your address";
+    public static final String INFO_STEP_1_DESCRIPTION =
+        "<html>Getting started is easy! Simply enter your delivery address<br>" +
+            "and feel free to save multiple addresses for added<br>" +
+            "convenience.</html>";
+    public static final String INFO_STEP_2_TITLE = "Step 2: Select a store";
+    public static final String INFO_STEP_2_DESCRIPTION =
+        "<html>Browse through our extensive selection of stores across<br>" +
+            "Greece and choose the one that suits your cravings best!</html>";
+    public static final String INFO_STEP_3_TITLE = "Step 3: Make your order";
+    public static final String INFO_STEP_3_DESCRIPTION =
+        "<html>Once you've selected your store, it's time to create your<br>" +
+            "order. Our easy-to-use interface ensures a hassle-free<br>" +
+            "ordering experience - and there are no extra charges to<br>" +
+            "worry about!</html>";
+
     // Invalid credentials alert
     public static final String INVALID_CREDENTIALS_TITLE = "Invalid Credentials";
     public static final String INVALID_CREDENTIALS_DESCRIPTION =
@@ -42,36 +62,36 @@ public final class UIConstants {
         "<html>There is an error with your input<br>"
             + "Please make sure that you have filled all the fields</html>";
 
-    
+
     //Message when user does not exist
     public static final String USER_NOT_EXIST_TITLE = "User does not exist";
-    public static final String USER_NOT_EXIST_DESCRIPTION = 
+    public static final String USER_NOT_EXIST_DESCRIPTION =
         "<html>This user does not exist<br>"
-        + "Maybe you are not signed up<br>"
-        + "Please sign up and then try again<br>"
-        + "If the problem continues please<br>"
-        + "contact our support for further assistance</html>";
-    
-    
+            + "Maybe you are not signed up<br>"
+            + "Please sign up and then try again<br>"
+            + "If the problem continues please<br>"
+            + "contact our support for further assistance</html>";
+
+
     //Message for wrong recovery answers
     public static final String WRONG_RECOVERY_ANSWERS_TITLE = "Wrong Answers";
-    public static final String WRONG_RECOVERY_ANSWERS_DESCRIPTION = 
+    public static final String WRONG_RECOVERY_ANSWERS_DESCRIPTION =
         "<html>You answered wrong. For security purposes<br>"
-        + "you will be redirected back to onboarding page.</html>";
+            + "you will be redirected back to onboarding page.</html>";
 
     //Message for wrong new password format
     public static final String WRONG_NEW_PASSWORD_FORMAT_TITLE = "Wrong New Password Format";
-    public static final String WRONG_NEW_PASSWORD_FORMAT_DESCRIPTION = 
+    public static final String WRONG_NEW_PASSWORD_FORMAT_DESCRIPTION =
         "<html>Please make sure the 2 fields have the same value<br>"
-        + "Also password must be at least 8 characters long</html>";
+            + "Also password must be at least 8 characters long</html>";
 
-    
+
     //Message for correct password change
     public static final String SUCCESSFUL_PASSWORD_CHANGE_TITLE = "Password changed successfully!";
-    public static final String SUCCESSFUL_PASSWORD_CHANGE_DESCRIPTION = 
+    public static final String SUCCESSFUL_PASSWORD_CHANGE_DESCRIPTION =
         "<html>You successfully changed your password!<br>"
-        + "You can continue to enjoy our services now!!!<br>"
-        + "You will be redirected to your home page</html>";
+            + "You can continue to enjoy our services now!!!<br>"
+            + "You will be redirected to your home page</html>";
 
     // Sign out alert
     public static final String SIGN_OUT_CONFIRM_MESSAGE = "Are you sure you want to sign out?";

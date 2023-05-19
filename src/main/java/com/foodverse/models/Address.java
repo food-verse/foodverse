0cf95bc9edc1c96f12f03f0f466f0cf315246aa2
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-05-19 20:05:10 +0300

    Fix formatting

--- src/main/java/com/foodverse/models/Address.java
+++ src/main/java/com/foodverse/models/Address.java
@@ -8,32 +8,27 @@ public record Address(
     String comments) {
 
     public Address withStreet(String street) {
-        if (this.street.equals(street))
-            return this;
+        if (this.street.equals(street)) return this;
         return new Address(street, number, floor, doorbell, comments);
     }
 
     public Address withNumber(String number) {
-        if (this.number.equals(number))
-            return this;
+        if (this.number.equals(number)) return this;
         return new Address(street, number, floor, doorbell, comments);
     }
 
     public Address withFloor(String floor) {
-        if (this.floor.equals(floor))
-            return this;
+        if (this.floor.equals(floor)) return this;
         return new Address(street, number, floor, doorbell, comments);
     }
 
     public Address withDoorbell(String doorbell) {
-        if (this.doorbell.equals(doorbell))
-            return this;
+        if (this.doorbell.equals(doorbell)) return this;
         return new Address(street, number, floor, doorbell, comments);
     }
 
     public Address withComments(String comments) {
-        if (this.comments.equals(comments))
-            return this;
+        if (this.comments.equals(comments)) return this;
         return new Address(street, number, floor, doorbell, comments);
     }
 

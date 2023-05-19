06e23c3f769a818d8b97eac9a6a8f2f690536fa2
georgiadisxc <128224291+georgiadisxc@users.noreply.github.com>
2023-05-19 17:25:08 +0300

    Update ShopOverlay with the newly added widgets

--- src/main/java/com/foodverse/overlays/ShopOverlay.java
+++ src/main/java/com/foodverse/overlays/ShopOverlay.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Font;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -20,30 +19,36 @@ import com.foodverse.utility.layout.EdgeInsets;
 import com.foodverse.utility.navigation.Overlay;
 import com.foodverse.utility.navigation.Router;
 import com.foodverse.utility.system.Database;
-import com.foodverse.utility.ui.Divider;
-import com.foodverse.utility.ui.ImageStyle;
 import com.foodverse.utility.ui.Button.ButtonSize;
 import com.foodverse.utility.ui.Button.ButtonType;
+import com.foodverse.utility.ui.Colors;
+import com.foodverse.utility.ui.Divider;
+import com.foodverse.utility.ui.ImageStyle;
 import com.foodverse.views.EmptyView;
 import com.foodverse.views.Offers;
-import com.foodverse.views.RateView;
+import com.foodverse.widgets.button.IconButton;
 import com.foodverse.widgets.button.PillButton;
 import com.foodverse.widgets.button.RectButton;
 import com.foodverse.widgets.layout.Column;
 import com.foodverse.widgets.layout.ListTile;
+import com.foodverse.widgets.layout.ListTile.ListTileSize;
 import com.foodverse.widgets.layout.Row;
 import com.foodverse.widgets.layout.ScrollView;
 import com.foodverse.widgets.media.IconAsset;
 import com.foodverse.widgets.media.Image;
-import com.foodverse.widgets.media.VectorImage;
+import com.foodverse.widgets.pickers.StarRating;
 import com.foodverse.widgets.text.Heading;
 import com.foodverse.widgets.text.Heading.HeadingSize;
+import com.foodverse.widgets.text.Label;
+import com.foodverse.widgets.text.Label.LabelSize;
+import com.foodverse.widgets.text.Paragraph;
+import com.foodverse.widgets.text.Paragraph.ParagraphSize;
 
 public final class ShopOverlay extends Overlay {
 
     // Getting a reference to the database...
     private final Database db = Database.getInstance();
-    // private final Component component;
+    // The name of the shop
     private final String merchant;
 
     public ShopOverlay(String merchant) {
@@ -58,17 +63,82 @@ public final class ShopOverlay extends Overlay {
         Optional<Shop> shop = db.findShopByName(merchant);
 
         // Getting the authenticated user...
-        Optional<User> signedUser = db.getAuthenticatedUser();
+        Optional<User> user = db.getAuthenticatedUser();
 
-        if (shop.isEmpty() || signedUser.isEmpty()) {
+        if (shop.isEmpty() || user.isEmpty()) {
             return new EmptyView().getRef();
         }
 
-        // pass the name of the shop in a variable
-        // var name = " ";
-        var name = shop.get().name();
+        //
+        var props = ShopProps.from(shop.get());
+        var signedUser = user.get();
 
-        var column = new Column();
+        var orderInfo = String.format(
+            "%s | %d’ | Minimum %.2f€",
+            props.type(),
+            props.prepTime(),
+            props.minOrder());
+
+        // Creating text widgets...
+        var shopNameText = new Heading(props.name(), HeadingSize.L);
+        var orderInfoText = new Label(orderInfo, LabelSize.M, Colors.gray600);
+
+        //
+        var favoritesButton = new IconButton(
+            IconAsset.HEART_FILL,
+            "Remove from favorites",
+            IconAsset.HEART,
+            "Add to favorites",
+            isEnabled -> {
+                if (isEnabled) {
+                    signedUser.favorites().remove(merchant);
+                    db.updateUser(signedUser);
+                } else {
+                    signedUser.favorites().add(merchant);
+                    db.updateUser(signedUser);
+                }
+            },
+            signedUser.favorites().contains(merchant));
+
+        //
+        var paddedFavoritesButton = new Column();
+        paddedFavoritesButton.addWidget(favoritesButton, new EdgeInsets.Builder()
+                .all(4)
+                .build(),
+            Align.FIRST_LINE_START);
+
+        //
+        var starRating = new StarRating(5, rating -> {
+            signedUser.ratings().put(merchant, rating);
+            db.updateUser(signedUser);
+        }, signedUser.ratings().getOrDefault(merchant, 0));
+
+        // Creating card's heading widget...
+        var headingWidget = new Row();
+        headingWidget.addWidget(shopNameText, new EdgeInsets.Builder()
+                .right(16)
+                .build(),
+            Align.CENTER);
+        headingWidget.addWidget(paddedFavoritesButton, Align.CENTER);
+
+        // Creating card's heading widget...
+        var headingWidgets = new Column();
+        headingWidgets.addWidget(headingWidget, new EdgeInsets.Builder()
+                .bottom(8)
+                .build(),
+            Align.FIRST_LINE_START);
+        headingWidgets.addWidget(orderInfoText, new EdgeInsets.Builder()
+                .bottom(8)
+                .build(),
+            Align.FIRST_LINE_START);
+        headingWidgets.addWidget(starRating, Align.FIRST_LINE_START);
+
+        //
+        var paddedHeading = new Column();
+        paddedHeading.addWidget(headingWidgets, new EdgeInsets.Builder()
+                .symmetric(24, 40)
+                .build(),
+            Align.FIRST_LINE_START);
 
         // Remove the default padding of the shop with a temporary panel
         var panelWithoutPad = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
@@ -79,116 +149,59 @@ public final class ShopOverlay extends Overlay {
         panel.setOpaque(false);
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-        // create heading with the name of the shop and put it in the panel
-        var nameHeading = new Heading(name, HeadingSize.L);
-        var namePanel = new JPanel();
-        namePanel.add(nameHeading.getRef());
-        namePanel.setBackground(Color.white);
-        var paddedHeading = new Row();
-        paddedHeading.addComponent(namePanel, new EdgeInsets.Builder()
-                .symmetric(40, 48)
-                .build(),
-            Align.CENTER);
-        panel.add(paddedHeading.getRef());
-        panel.add(namePanel);
-
-        //
-        var props = ShopProps.from(shop.get());
-
         // Creating image widgets...
-        var thumbnailImage = new Image(props.thumbnail(),
-            new ImageStyle.Builder().width(800).height(200).build());
-        var heartImage = new VectorImage(IconAsset.HEART);
-        var heartFillImage = new VectorImage(IconAsset.HEART_FILL);
+        var thumbnailImage = new Image(props.thumbnail(), new ImageStyle.Builder()
+            .width(800)
+            .height(200)
+            .build());
 
-        // Create a map for the choosen items
+        // Create a map for the chosen items
         Map<String, Integer> addProducts = new HashMap<>();
 
-        // Add padding to heart
         var paddedImage = new Column();
-        paddedImage.addWidget(thumbnailImage, new EdgeInsets.Builder().all(0).build(),
+        paddedImage.addWidget(thumbnailImage, new EdgeInsets.Builder()
+                .all(0)
+                .build(),
             Align.FIRST_LINE_START);
-
-        var row = new Row();
-
-        if (signedUser.get().favorites().contains(merchant)) {
-            row.addWidget(heartFillImage,
-                new EdgeInsets.Builder()
-                    .all(4)
-                    .build(),
-                Align.FIRST_LINE_START);
-        } else {
-            row.addWidget(heartImage,
-                new EdgeInsets.Builder()
-                    .all(4)
-                    .build(),
-                Align.FIRST_LINE_START);
-        }
-
-        heartImage.onPressed(e -> {
-            row.replaceWidget(heartImage, heartFillImage);
-            signedUser.get().favorites().add(merchant);
-            db.updateUser(signedUser.get());
-        }, "Add to favorites");
-
-        heartFillImage.onPressed(e -> {
-            row.replaceWidget(heartFillImage, heartImage);
-            signedUser.get().favorites().remove(merchant);
-            db.updateUser(signedUser.get());
-        }, "Remove from favorites");
-
-        panel.add(row.getRef());
-
-        // preparation time
-        var prepTime = new ListTile(
-            shop.get().type() + " " + props.prepTime() + "'  |  Minimum " + props.minOrder()
-                + "$");
-        panel.add(prepTime.getRef());
 
         // Shop's address
         panel.add(new Divider());
-        var shopAddress = new JLabel(props.address());
+        var shopAddress = new Paragraph(props.address(), ParagraphSize.L);
         var paddedAddress = new Row();
-        paddedAddress.addComponent(shopAddress, new EdgeInsets.Builder().top(0).build(),
+        paddedAddress.addWidget(shopAddress, new EdgeInsets.Builder()
+                .top(0)
+                .build(),
             Align.CENTER);
-        changeFont(shopAddress, "Arial", Font.PLAIN, 16);
-        panel.add(shopAddress);
+        panel.add(paddedAddress.getRef());
         panel.add(new Divider());
 
         // offers
-        var offersTile = new ListTile("Offers");
+        var offersTile = new ListTile("Offers", ListTileSize.S);
 
         panel.add(offersTile.getRef());
-        // offersTile.setFont(new Font("Courier New", Font.BOLD, 25));
-        // offersTile.setForeground(Color.black);
 
-        if (shop.get().offers().isEmpty()) {
+        if (props.offers().isEmpty()) {
             panel.add(new EmptyView().getRef());
         } else {
             panel.add(new Offers(shop).getRef());
         }
 
-        // props.offers();
-
         // Menu
-        var menuTile = new ListTile("Menu");
-        // var menu = new JLabel("Menu ");
-        // menuTitle.setFont(new Font("Courier New", Font.BOLD, 25));
-        // menutitle.setForeground(Color.black);
+        var menuTile = new ListTile("Menu", ListTileSize.S);
 
         panel.add(menuTile.getRef());
 
-        for (int i = 0; i < shop.get().menu().size(); i++) {
+        for (int i = 0; i < props.menu().size(); i++) {
 
             var itemPanel = new JPanel();
 
             itemPanel.setPreferredSize(new Dimension(200, 40));
 
-            var x = shop.get().menu().get(i).name();
-            var price = shop.get().menu().get(i).price();
+            var x = props.menu().get(i).name();
+            var price = props.menu().get(i).price();
             var priceAsAString = Float.toString(price);
             var menuName = new JLabel(x);
-            var menuPrice = new JLabel(priceAsAString + " \u20AC");
+            var menuPrice = new JLabel(priceAsAString + " €");
 
             var productAddButton = new PillButton("Add", ButtonSize.XS, ButtonType.SECONDARY, e -> {
                 addProducts.put(x, 1);
@@ -212,16 +225,9 @@ public final class ShopOverlay extends Overlay {
                     .build(),
                 Align.CENTER);
             panel.add(paddedMenu.getRef());
-
             panel.add(itemPanel);
 
         }
-
-        panel.setBackground(Color.white);
-
-        // Rate
-        RateView rate = new RateView(shop);
-        panel.add(rate.getRef());
 
         // RectButton "Cart" to appear the OrderOverlay
         var OrderButton = new RectButton("Cart", ButtonSize.L, ButtonType.PRIMARY, e -> {
@@ -231,20 +237,14 @@ public final class ShopOverlay extends Overlay {
         });
 
         panel.add(OrderButton.getRef());
-
-        // component = new ScrollView(panel).getRef();
-
+        var column = new Column();
         column.addWidget(thumbnailImage, new EdgeInsets.Builder().all(0).build(),
             Align.FIRST_LINE_START);
+        column.addWidget(paddedHeading, Align.FIRST_LINE_START);
         column.addComponent(panel, new EdgeInsets.Builder().left(150).build(),
             Align.LAST_LINE_START);
-        // return column.getRef();
-        return new ScrollView(column.getRef()).getRef();
-    }
 
-    public static void changeFont(JLabel label, String fontName, int fontStyle, int fontSize) {
-        Font font = new Font(fontName, fontStyle, fontSize);
-        label.setFont(font);
+        return new ScrollView(column.getRef()).getRef();
     }
 
 }

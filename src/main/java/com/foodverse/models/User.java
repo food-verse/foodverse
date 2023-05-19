1be7085607d53e9ee7c8bd3d8edbdd558f0935d2
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-05-19 20:06:26 +0300

    Use new Address class in User

--- src/main/java/com/foodverse/models/User.java
+++ src/main/java/com/foodverse/models/User.java
@@ -3,18 +3,19 @@ package com.foodverse.models;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 
 import com.google.gson.annotations.SerializedName;
 
 public record User(
     @SerializedName("name") String name,
-    @SerializedName("addresses") List<String> addresses,
+    @SerializedName("addresses") List<Address> addresses,
     @SerializedName("phone") String phone,
     @SerializedName("email") String email,
     @SerializedName("credentials") Credentials credentials,
-    @SerializedName("favorites") List<String> favorites,
+    @SerializedName("favorites") Set<String> favorites,
     @SerializedName("rating") Map<String, Integer> ratings,
-    @SerializedName("orders") List<Order> orders) {
+    @SerializedName("orders") Set<Order> orders) {
 
     public User withName(String name) {
         return new User(name, addresses, phone, email, credentials, favorites, ratings, orders);

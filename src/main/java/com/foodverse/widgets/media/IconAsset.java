4aa8907c6389dd470f55f9b0257941431e6e9525
georgiadisxc <128224291+georgiadisxc@users.noreply.github.com>
2023-05-22 01:00:30 +0300

    Add two new icons for order overlay

--- src/main/java/com/foodverse/widgets/media/IconAsset.java
+++ src/main/java/com/foodverse/widgets/media/IconAsset.java
@@ -3,9 +3,11 @@ package com.foodverse.widgets.media;
 public enum IconAsset {
     AVATAR("avatar.svg"),
     BRAND("brand.svg"),
+    CREDIT_CARD("credit_card.svg"),
     HEART_FILL("heart-fill.svg"),
     HEART("heart.svg"),
     LOCATION("location.svg"),
+    PAYMENTS("payments.svg"),
     STAR_MEDIUM_FILL("star_medium_fill.svg"),
     STAR_MEDIUM_OUTLINE("star_medium_outline.svg"),
     STAR_SMALL_FILL("star_small_fill.svg");

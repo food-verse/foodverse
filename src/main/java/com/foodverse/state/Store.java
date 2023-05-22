b2e32c51c76bbc704fbf5f4e489bf5b64bfc7350
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-05-22 13:04:35 +0300

    Update Store class to include orderDeliveryTip

--- src/main/java/com/foodverse/state/Store.java
+++ src/main/java/com/foodverse/state/Store.java
@@ -9,4 +9,6 @@ public final class Store {
 
     public static final State<PaymentMethod> orderPaymentMethod = new State<>(PaymentMethod.CARD);
 
+    public static final State<Double> orderDeliveryTip = new State<>(0.0);
+
 }

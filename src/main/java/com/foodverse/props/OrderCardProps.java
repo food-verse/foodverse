c769bc837be220d0ff72c65729a7e34b14b37723
georgiadisxc <128224291+georgiadisxc@users.noreply.github.com>
2023-05-22 13:00:00 +0300

    Update props classes in order to reflect new changes to order and shop classes

--- src/main/java/com/foodverse/props/OrderCardProps.java
+++ src/main/java/com/foodverse/props/OrderCardProps.java
@@ -2,30 +2,35 @@ package com.foodverse.props;
 
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
-import com.foodverse.models.Order;
-import com.foodverse.models.Shop;
+import com.foodverse.models.*;
 import com.foodverse.utility.core.Props;
 import com.foodverse.utility.system.Database;
 import com.foodverse.widgets.media.AssetSize;
 
 public record OrderCardProps(
+
     String thumbnail,
     String name,
+    UUID id,
     Map<String, Integer> items,
-    float price) implements Props {
+    double price) implements Props {
 
     public static OrderCardProps from(Order order) {
         Optional<Shop> foundShop = Database.getInstance().findShopByName(order.merchant());
-        if (foundShop.isPresent()) {
-            return new OrderCardProps(
-                foundShop.get().thumbnails().get(AssetSize.MEDIUM),
-                foundShop.get().name(),
-                order.items(),
-                order.total());
-        } else {
-            return new OrderCardProps("", "", order.items(), order.total());
-        }
+        return foundShop.map(shop -> new OrderCardProps(
+            shop.thumbnails().get(AssetSize.MEDIUM),
+            shop.name(),
+            order.id(),
+            order.items(),
+            order.total())).orElseGet(() -> new OrderCardProps(
+            "",
+            "",
+            UUID.randomUUID(),
+            order.items(),
+            order.total())
+        );
     }
 
 }

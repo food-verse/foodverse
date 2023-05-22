25649fa1fd00bae63b4f55cbf9aee11aec56884d
georgiadisxc <128224291+georgiadisxc@users.noreply.github.com>
2023-05-22 12:57:55 +0300

    Override toString in PaymentMethod enum

--- src/main/java/com/foodverse/models/PaymentMethod.java
+++ src/main/java/com/foodverse/models/PaymentMethod.java
@@ -1,5 +1,12 @@
 package com.foodverse.models;
 
 public enum PaymentMethod {
-    CARD, CASH
+    CARD, CASH;
+
+    @Override
+    public String toString() {
+        String name = name().toLowerCase();
+        return name.substring(0, 1).toUpperCase() + name.substring(1);
+    }
+
 }

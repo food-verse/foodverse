20067bd0b3f46715295827b5f4a687609e0b43e0
georgiadisxc <128224291+georgiadisxc@users.noreply.github.com>
2023-06-02 22:20:42 +0300

    Create new TextArea component

---
+++ src/main/java/com/foodverse/utility/ui/TextArea.java
@@ -0,0 +1,81 @@
+package com.foodverse.utility.ui;
+
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Shape;
+import java.awt.geom.RoundRectangle2D;
+import java.io.Serial;
+import javax.swing.BorderFactory;
+import javax.swing.JTextArea;
+
+import com.foodverse.utility.ui.Button.ButtonSize;
+import com.foodverse.widgets.button.RectButton;
+
+public final class TextArea extends JTextArea {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private Shape shape;
+    private final ButtonTheme theme = new ButtonTheme.Builder()
+        .defaultBackgroundColor(Colors.buttonSecondaryFill)
+        .defaultTextColor(Colors.buttonSecondaryText)
+        .hoverBackgroundColor(Colors.buttonSecondaryHover)
+        .hoverTextColor(Colors.buttonSecondaryText)
+        .pressBackgroundColor(Colors.buttonSecondaryActive)
+        .pressTextColor(Colors.buttonSecondaryText)
+        .build();
+    private boolean isEnabled = true;
+    private final ButtonStyle buttonStyle = RectButton.getButtonStyle(
+        true,
+        ButtonSize.S,
+        theme,
+        false);
+
+    public TextArea() {
+        setFont(new Font(buttonStyle.getFontStyle().getAttributes()));
+        setBorder(BorderFactory.createEmptyBorder(8, 16, 8, 16));
+        setOpaque(false);
+        setEnabled(isEnabled);
+        setSelectionColor(Colors.buttonSecondaryActive);
+        setColumns(56);
+        setRows(2);
+        applyStyle();
+    }
+
+    public void toggle() {
+        isEnabled = !isEnabled;
+        setEnabled(isEnabled);
+        applyStyle();
+    }
+
+    public void applyStyle() {
+        if (isEnabled) {
+            setForeground(buttonStyle.getDefaultTextColor());
+            setBackground(buttonStyle.getDefaultBackgroundColor());
+        } else {
+            setBackground(buttonStyle.getDisabledBackgroundColor());
+            setForeground(buttonStyle.getDisabledTextColor());
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        g.setColor(getBackground());
+        g.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 8, 8);
+        super.paintComponent(g);
+    }
+
+    @Override
+    public boolean contains(int x, int y) {
+        if (shape == null || !shape.getBounds().equals(getBounds())) {
+            shape = new RoundRectangle2D.Float(0, 0, getWidth() - 1, getHeight() - 1, 8, 8);
+        }
+        return shape.contains(x, y);
+    }
+
+    @Override
+    protected void paintBorder(Graphics g) {
+        // Skip painting border
+    }
+
+}

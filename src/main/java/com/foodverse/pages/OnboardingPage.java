aa348b9e0979b5d0961b9c1fe2854a7fc08f7d9f
georgiadisxc <128224291+georgiadisxc@users.noreply.github.com>
2023-06-06 12:08:06 +0300

    Reland info button now that the application is production ready

--- src/main/java/com/foodverse/pages/OnboardingPage.java
+++ src/main/java/com/foodverse/pages/OnboardingPage.java
@@ -6,7 +6,7 @@ import java.util.Random;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
-
+import com.foodverse.overlays.InfoOverlay;
 import com.foodverse.overlays.SignInOverlay;
 import com.foodverse.overlays.SignUpOverlay;
 import com.foodverse.utility.common.Endpoints;
@@ -16,9 +16,7 @@ import com.foodverse.utility.common.URLHandler;
 import com.foodverse.utility.layout.Align;
 import com.foodverse.utility.layout.EdgeInsets;
 import com.foodverse.utility.navigation.Page;
-import com.foodverse.utility.navigation.Pages;
 import com.foodverse.utility.navigation.Router;
-import com.foodverse.utility.system.Database;
 import com.foodverse.utility.ui.Button.ButtonSize;
 import com.foodverse.utility.ui.Button.ButtonType;
 import com.foodverse.utility.ui.Colors;
@@ -46,21 +44,20 @@ public final class OnboardingPage extends Page {
     public OnboardingPage() {
 
         onboardingImages = List.of(
-            "corn_meat_quesadillas_sauce_on_board.jpg",
-            "mexican_fajita_nachos_sombrero_pan.jpg",
-            "tasty_fajita_with_veggies_beef.jpg"
-        );
+                "corn_meat_quesadillas_sauce_on_board.jpg",
+                "mexican_fajita_nachos_sombrero_pan.jpg",
+                "tasty_fajita_with_veggies_beef.jpg");
 
         // Creating text widgets...
         var promoTitleText = new Display(UIConstants.ONBOARDING_PROMO_TITLE, DisplaySize.XS);
         var promoMessageText = new Heading(
-            UIConstants.ONBOARDING_PROMO_MESSAGE,
-            HeadingSize.S,
-            Colors.gray600);
+                UIConstants.ONBOARDING_PROMO_MESSAGE,
+                HeadingSize.S,
+                Colors.gray600);
         var promoNoteText = new Heading(
-            UIConstants.ONBOARDING_PROMO_NOTE,
-            HeadingSize.XS,
-            Colors.gray600);
+                UIConstants.ONBOARDING_PROMO_NOTE,
+                HeadingSize.XS,
+                Colors.gray600);
 
         // Creating image widgets...
         var repositoryUrl = Endpoints.REPOSITORY.getLink();
@@ -68,31 +65,28 @@ public final class OnboardingPage extends Page {
             URLHandler.open(repositoryUrl);
         }, repositoryUrl);
         var hushFaceImage = new Image("hush-emoji.png", new ImageStyle.Builder()
-            .width(32)
-            .height(32)
-            .build());
+                .width(32)
+                .height(32)
+                .build());
 
         // Creating button widgets...
-        // TODO: REVERT
         var infoButton = new PillButton(
-            "How it works",
-            ButtonSize.M,
-            ButtonType.TERTIARY,
-            e -> {
-                var db = Database.getInstance();
-                db.signIn("emilysmith123@gmail.com", "XyZ987!");
-                Router.pushPage(Pages.HOME);
-            });
+                "How it works",
+                ButtonSize.M,
+                ButtonType.TERTIARY,
+                e -> {
+                    Router.openOverlay(new InfoOverlay());
+                });
         var signInButton = new PillButton(
-            "Sign In",
-            ButtonSize.M,
-            ButtonType.SECONDARY,
-            e -> Router.openOverlay(new SignInOverlay()));
+                "Sign In",
+                ButtonSize.M,
+                ButtonType.SECONDARY,
+                e -> Router.openOverlay(new SignInOverlay()));
         var signUpButton = new PillButton(
-            "Sign Up",
-            ButtonSize.M,
-            ButtonType.PRIMARY,
-            e -> Router.openOverlay(new SignUpOverlay()));
+                "Sign Up",
+                ButtonSize.M,
+                ButtonType.PRIMARY,
+                e -> Router.openOverlay(new SignUpOverlay()));
 
         // Creating main panel...
         panel = new JPanel();
@@ -110,11 +104,11 @@ public final class OnboardingPage extends Page {
                 .left(584)
                 .right(16)
                 .build(),
-            Align.BOTTOM_RIGHT);
+                Align.BOTTOM_RIGHT);
         buttonRow.addWidget(signInButton, new EdgeInsets.Builder()
                 .right(16)
                 .build(),
-            Align.BOTTOM_RIGHT);
+                Align.BOTTOM_RIGHT);
         buttonRow.addWidget(signUpButton, Align.BOTTOM_RIGHT);
 
         // Add images to the parent panel
@@ -128,14 +122,14 @@ public final class OnboardingPage extends Page {
                 .symmetric(40, 48)
                 .bottom(36)
                 .build(),
-            Align.CENTER);
+                Align.CENTER);
         panel.add(paddedHeading.getRef());
 
         var promoText = new Column();
         promoText.addWidget(promoTitleText, new EdgeInsets.Builder()
                 .bottom(16)
                 .build(),
-            Align.TOP_LEFT);
+                Align.TOP_LEFT);
         promoText.addWidget(promoMessageText, Align.BOTTOM_RIGHT);
 
         var noteWidget = new Row();
@@ -144,9 +138,9 @@ public final class OnboardingPage extends Page {
 
         textWidgets = new Column();
         textWidgets.addWidget(promoText, new EdgeInsets.Builder()
-            .top(164)
-            .bottom(172)
-            .build());
+                .top(164)
+                .bottom(172)
+                .build());
         textWidgets.addWidget(noteWidget, new EdgeInsets.Builder().build());
 
     }
@@ -156,11 +150,11 @@ public final class OnboardingPage extends Page {
 
         // Creating the onboarding image widget
         var onboardingImage = new Image(
-            onboardingImages.get(random.nextInt(onboardingImages.size())),
-            new ImageStyle.Builder()
-                .width(400)
-                .height(560)
-                .build());
+                onboardingImages.get(random.nextInt(onboardingImages.size())),
+                new ImageStyle.Builder()
+                        .width(400)
+                        .height(560)
+                        .build());
 
         // Remove the last component (content) if the page has already rendered once
         if (!didRender) {
@@ -173,16 +167,16 @@ public final class OnboardingPage extends Page {
         // Add the text widgets and the onboarding image to the content row
         var content = new Row();
         content.addWidget(textWidgets, new EdgeInsets.Builder()
-            .right(432)
-            .build());
+                .right(432)
+                .build());
         content.addWidget(onboardingImage);
 
         // Add the padded content row to the main panel
         var paddedContent = new Column();
         paddedContent.addWidget(content, new EdgeInsets.Builder()
-            .all(48)
-            .top(0)
-            .build());
+                .all(48)
+                .top(0)
+                .build());
         panel.add(paddedContent.getRef());
 
         return panel;
